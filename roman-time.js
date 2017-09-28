bc1e8bcf034bf21161de0866a7d58b03860986b1
eyebrow elite <wivoceraku@p33.org>
2017-09-28 23:02:19 +0500

    + golf

--- roman-time.js
+++ roman-time.js
@@ -1,13 +1,9 @@
 'use strict';
-const zlib = require('zlib');
-const timeRegex = /^(\d{2}):(\d{2})$/;
-const romanDigitsCompressed = 'eJw1i8kJADEMA//qTGDyDO6/ktWRJdIY4vEBQUe90KOS' +
-    '6mOhRyU1LtahW/hXlkO3CGVuyuAxK+spg8cOn2zB8h/d624UukWoi0kZPGa1GMi3HtvyOETMivH2A74sRWM=';
-
-var romanDigits = zlib
-    .inflateSync(new Buffer(romanDigitsCompressed, 'base64'))
-    .toString()
-    .split('\n');
+const timeRegex = /^(\d\d):(\d\d)$/;
+const romanNumbers = ('N I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII ' +
+'XIX XX XXI XXII XXIII XXIV XXV XXVI XXVII XXVIII XXIX XXX XXXI XXXII XXXIII XXXIV XXXV ' +
+'XXXVI XXXVII XXXVIII XXXIX XL XLI XLII XLIII XLIV XLV XLVI XLVII XLVIII XLIX L LI LII ' +
+'LIII LIV LV LVI LVII LVIII LIX LX').split(' ');
 
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
@@ -15,13 +11,12 @@ var romanDigits = zlib
  */
 function romanTime(time) {
     var groups = timeRegex.exec(time);
-    var hour = (groups || [])[1] - 0;
-    var minute = (groups || [])[2] - 0;
+    var [hour, minute] = (groups || []).slice(1, 3).map(x => x - 0);
     if (!groups || hour > 23 || minute > 59) {
         throw new TypeError();
     }
 
-    return [hour, minute].map(x => romanDigits[x]).join(':');
+    return [hour, minute].map(x => romanNumbers[x]).join(':');
 }
 
 module.exports = romanTime;

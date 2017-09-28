4ca9f1b9dfddf215cf15e190276b55cd51c145f1
Denis Mukhametov <denis.mukhametov@gmail.com>
2017-09-29 01:51:01 +0500

    Style changes

--- roman-time.js
+++ roman-time.js
@@ -6,7 +6,9 @@ function error() {
 
 function parseNumber(text, limit) {
     let number = parseInt(text, 10);
-    if (Number.isNaN(number)) error();
+    if (Number.isNaN(number)) {
+        error();
+    }
     if (number < 0 || number > limit) {
         error();
     } else {
@@ -15,31 +17,35 @@ function parseNumber(text, limit) {
 }
 
 function parseTime(time) {
-    if (typeof time !== "string") error();
+    if (typeof time !== 'string') {
+        error();
+    }
     let numbersList = time.split(':');
-    if (numbersList.length !== 2) error();
+    if (numbersList.length !== 2) {
+        error();
+    }
+
     return [23, 59].map((limit, index) => parseNumber(numbersList[index], limit));
 }
 
 function convertTime(number) {
-    if (number == 0) return 'N';
+    if (number === 0) {
+        return 'N';
+    }
     let arab = [1, 4, 5, 9, 10, 40, 50];
-    let roman = ['I','IV','V','IX','X','XL','L'];
+    let roman = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
     let result = '';
-	let i = arab.length - 1;
-    while(number > 0)
-	{
-		if(number >= arab[i])
-		{
-			result += roman[i];
-			number -= arab[i];
-		}
-		else
-		{
-			i--;
-		}
-	}
-	return result;
+    let i = arab.length - 1;
+    while (number > 0) {
+        if (number >= arab[i]) {
+            result += roman[i];
+            number -= arab[i];
+        } else {
+            i--;
+        }
+    }
+
+    return result;
 }
 
 /**
@@ -48,7 +54,9 @@ function convertTime(number) {
  */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    return parseTime(time).map(convertTime).join(':');
+    return parseTime(time)
+    .map(convertTime)
+    .join(':');
 }
 
 module.exports = romanTime;

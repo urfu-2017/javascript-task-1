12081473a3df7994d1d0c5537eb260a2044163a6
Dorokhova Tatiana <t.dorohovav@gmail.com>
2017-09-29 03:41:31 +0500

    fix[2]

--- roman-time.js
+++ roman-time.js
@@ -10,7 +10,7 @@ var ARABIC_NUMERALS = ['1', '4', '5', '9', '10', '40', '50'];
 
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    if (typeof time !== 'string' || !isValitedString) {
+    if (!isValitedString) {
         throw new TypeError();
     }
 
@@ -30,10 +30,10 @@ function isValitedString(time) {
 
 function convertNumber(number, maxValue) {
     number = parseInt(number, 10);
-    if (isNaN(number) || (number > maxValue || number < 0)) {
+    if (isNaN(number) || (number > maxValue) || (number < 0)) {
         throw new TypeError();
     }
-    if (!number) {
+    if (number === 0) {
         return 'N';
     }
 

79f9e5d5b6b39e163a2e13ad83c659b8e8e8d340
Behemothhh <elakov111@gmail.com>
2017-09-28 23:41:37 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -5,8 +5,26 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
+    var arrayOfArab = [1,4,5,9,10,40,50,0];
+    var arrayOfRom = ["I","IV","V","IX","X","XL","L","N"];
+    var result = ['', ''];
+    var timeSplit = time.split(":");
+    if (timeSplit.length > 2) throw new TypeError("Слишком много разрядов");
+    for (var i = 0; i < timeSplit.length; i++) {
+        timeSplit[i] = parseInt(timeSplit[i], 10);
+        if (isNaN(timeSplit[i])) throw new TypeError("Введите числа");
+        else if (timeSplit[i] > (i==0 ? 23 : 59) || timeSplit[i] < 0) throw new TypeError("Неправильные числа");
+        var n = arrayOfArab.length - 1;
+        while (timeSplit[i] >= 0) {
+            if (timeSplit[i] >= arrayOfArab[n]) {
+                result[i] += arrayOfRom[n];
+                timeSplit[i] -= arrayOfRom[n] == 0 ? arrayOfRom[n] : 1;
+            }
+            else n--;
+        }
+    }
     return time;
 }
+
 
 module.exports = romanTime;

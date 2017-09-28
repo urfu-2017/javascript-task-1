b6a57dc2305701ff78c9a5271b2e430c5bee77e3
eyebrow elite <wivoceraku@p33.org>
2017-09-28 23:22:47 +0500

    + golf2

--- roman-time.js
+++ roman-time.js
@@ -1,9 +1,9 @@
 'use strict';
 const timeRegex = /^(\d\d):(\d\d)$/;
-const romanNumbers = ('N I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII ' +
-'XIX XX XXI XXII XXIII XXIV XXV XXVI XXVII XXVIII XXIX XXX XXXI XXXII XXXIII XXXIV XXXV ' +
-'XXXVI XXXVII XXXVIII XXXIX XL XLI XLII XLIII XLIV XLV XLVI XLVII XLVIII XLIX L LI LII ' +
-'LIII LIV LV LVI LVII LVIII LIX LX').split(' ');
+const romanNumbers = `N I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI XVII XVIII
+XIX XX XXI XXII XXIII XXIV XXV XXVI XXVII XXVIII XXIX XXX XXXI XXXII XXXIII XXXIV XXXV
+XXXVI XXXVII XXXVIII XXXIX XL XLI XLII XLIII XLIV XLV XLVI XLVII XLVIII XLIX L LI LII
+LIII LIV LV LVI LVII LVIII LIX LX`.split(/\s/g);
 
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
@@ -11,7 +11,7 @@ const romanNumbers = ('N I II III IV V VI VII VIII IX X XI XII XIII XIV XV XVI X
  */
 function romanTime(time) {
     var groups = timeRegex.exec(time);
-    var [hour, minute] = (groups || []).slice(1, 3).map(x => x - 0);
+    var [, hour, minute] = (groups || []).map(x => x++);
     if (!groups || hour > 23 || minute > 59) {
         throw new TypeError();
     }

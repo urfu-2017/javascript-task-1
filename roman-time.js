38ce9114c3ba78a2ae02e9253e8f30e39b010fdc
Ilya Kulygin <ruserixsian@gmail.com>
2017-09-28 21:56:22 +0500

    Try #5

--- roman-time.js
+++ roman-time.js
@@ -1,22 +1,8 @@
 'use strict';
 
-const tens = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
-const ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
-
-const convertToRoman = (number) => {
-    const indexOfTens = parseInt(number / 10, 10);
-    const indexOfOnes = parseInt(number % 10, 10);
-
-    if (indexOfTens === 0 && indexOfOnes === 0) {
-        return 'N';
-    }
-
-    return tens[indexOfTens] + ones[indexOfOnes];
-};
-
 const isValidNumber = (value, a, b) => isFinite(value) && value >= a && value <= b;
 
-const getNumbers = (time) => {
+const parseTime = (time) => {
     if (typeof time !== 'string' || time.length > 5) {
         throw new TypeError();
     }
@@ -32,15 +18,29 @@ const getNumbers = (time) => {
     return { hours, minutes };
 };
 
+const tens = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+const ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+
+const convertToRoman = (number) => {
+    const indexOfTens = parseInt(number / 10, 10);
+    const indexOfOnes = parseInt(number % 10, 10);
+
+    if (indexOfTens === 0 && indexOfOnes === 0) {
+        return 'N';
+    }
+
+    return tens[indexOfTens] + ones[indexOfOnes];
+};
+
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    const date = getNumbers(time);
+    const parsedTime = parseTime(time);
 
-    const romanHours = convertToRoman(date.hours);
-    const romanMinutes = convertToRoman(date.minutes);
+    const romanHours = convertToRoman(parsedTime.hours);
+    const romanMinutes = convertToRoman(parsedTime.minutes);
 
     return `${romanHours}:${romanMinutes}`;
 }

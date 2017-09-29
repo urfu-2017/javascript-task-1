af0e9850143ec0c1dddfab4fcec33609fb15a798
Behemothhh <elakov111@gmail.com>
2017-09-29 09:42:39 +0500

    убрал глупые ошибки

--- roman-time.js
+++ roman-time.js
@@ -5,17 +5,18 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    if (/[^0-9:]/i.test(time)) throw new TypeError('Используются недопустимые символы');
+    if (/[^0-9:]/i.test(time)) {
+        throw new TypeError('Используются недопустимые символы');
+    }
     var i = 0;
     var arrayOfRom = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X',
                      'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX',
                      'XX', 'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI', 'XXVII',
                      'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII', 'XXXIII', 'XXXIV',
-                     'XXXV', 'XXXVI', 'XXXVII', 'XXXVIII', 'XXXIX', 'XL', 'XLI', 
-                     'XLII', 'XLIII', 'XLIV', 'XLV', 'XLVI', 'XLVII', 'XLVIII', 
-                     'XLIX', 'L', 'LI', 'LII', 'LIII', 'LIV', 'LV', 'LVI', 
+                     'XXXV', 'XXXVI', 'XXXVII', 'XXXVIII', 'XXXIX', 'XL', 'XLI',
+                     'XLII', 'XLIII', 'XLIV', 'XLV', 'XLVI', 'XLVII', 'XLVIII',
+                     'XLIX', 'L', 'LI', 'LII', 'LIII', 'LIV', 'LV', 'LVI',
                      'LVII', 'LVIII', 'LIX'];
-    var result = ['', ''];
     var timeSplit = time.split(':');
     if (timeSplit.length > 2) {
         throw new TypeError('Слишком много разрядов');

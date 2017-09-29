c74280c3f6420d041a349120d54109fcc1191a43
Hazipov Vladislav <hazipov.vlad@yandex.ru>
2017-09-30 01:22:02 +0500

    Rome

--- roman-time.js
+++ roman-time.js
@@ -5,29 +5,49 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    try { 
-        let ourTime = time.split(':');
-        if(ourTime[0]>23||ourTime[0]<0||ourTime[1]>59||ourTime[1]<0||isNaN(ourTime[0])||isNaN(ourTime[1])||ourTime[0]===null||ourTime[1]===null) {
-            throw new TypeError('TypeError: Неверное время');
-            return;
-        }
-        time = toRoman(ourTime[0])+":"+toRoman(ourTime[1]);
-        return time;
-    } catch(e) {
-       console.log(e.message); 
-    }  
+
+    let ourTime = time.split(':');
+
+    if (checkTime(ourTime) || checkTimeOnStrange(ourTime)) {
+        throw new TypeError('Неверное время');
+    }
+
+    time = toRoman(ourTime[0]) + ':' + toRoman(ourTime[1]);
+
+    return time;
+
 }
 
 function toRoman(num) {
 
-    let Romans = ['','X','XX','XXX','XXXX','L'];
-    let smallRomans = ['','I',"II","III",'IV','V','VI','VII','VIII','IX'];
-    if(num === '00') return 'N'
+    let Romans = ['', 'X', 'XX', 'XXX', 'XXXX', 'L'];
+    let smallRomans = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    if (num === '00') {
+
+        return 'N';
+    }
     num = Romans[Number(num[0])] + smallRomans[Number(num[1])];
 
-return num;
+    return num;
 }
 
-romanTime('24:00');
+function checkTime(ourTime) {
+
+    if (ourTime[0] > 23 || ourTime[0] < 0 || ourTime[1] > 59 || ourTime[1] < 0) {
+
+        return true;
+    }
+
+    return false;
+}
+
+function checkTimeOnStrange(ourTime) {
+
+    if (isNaN(ourTime[0]) || isNaN(ourTime[1]) || ourTime[0] === null || ourTime[1] === null) {
+        return true;
+    }
+
+    return false;
+}
 
 module.exports = romanTime;

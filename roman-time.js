58eab7fb21b43f7c1644414193e00c7230d7d76a
darl0ck <danilok97@gmail.com>
2017-09-30 01:10:22 +0500

    #1

--- roman-time.js
+++ roman-time.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 'use strict';
 
 /**
@@ -6,7 +7,28 @@
  */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    return time;
+    const Roman1to9 = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XL', 'L'];
+    const Roman10 = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+    let t = time.split(':', 2);
+    let min = parseInt(t[1], 10);
+    let hour = parseInt(t[0], 10);
+    if (min > 59 || min < 0 || hour > 23 || hour < 0 || min === NaN || hour === NaN) {
+        console.log('TypeError: Неверное время');  
+    } else {
+        let romanMin = Roman10[Math.floor(min / 10)] + Roman1to9[min % 10];
+        let romanHour = Roman10[Math.floor(hour / 10)] + Roman1to9[hour % 10];
+        
+        if (min === 0 && hour !==0){
+            romanMin = 'N';
+        } else if (hour === 0 && min !==0 ) {
+            romanHour = 'N';
+        } else if (min === 0 && hour === 0) {
+            romanHour = 'N';
+            romanMin = 'N';
+        }
+        console.log(`${romanHour}:${romanMin}`);
+    }
+    
 }
 
 module.exports = romanTime;

8536b9ebe504f5c0fca664601265dd82e0cbf2f5
qmzik <acke208@yandex.ru>
2017-09-30 01:07:32 +0500

    task has been finished

--- roman-time.js
+++ roman-time.js
@@ -5,7 +5,64 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
+
+    let romanNumbers = [
+        'N',
+        'I',
+        'IV',
+        'V',
+        'IX',
+        'X',
+        'XL',
+        'L'
+    ];
+
+    let presentNumbers = [
+        0,
+        1,
+        4,
+        5,
+        9,
+        10,
+        40,
+        50
+    ];
+
+    let hoursAndMinuts = time.split(':');
+
+    // Варианты неправильного ввода
+    if (hoursAndMinuts.length > 2 ||
+        hoursAndMinuts.forEach((string) => (string.isNaN)) ||
+        hoursAndMinuts[0] > 23 || hoursAndMinuts > 59) {
+        throw new TypeError('Такого времени не может быть');
+    }
+
+
+    hoursAndMinuts = hoursAndMinuts.map((number) => {
+
+        number = parseInt(number);
+        let iter = presentNumbers.length - 1;
+        let result = '';
+
+        if (number === 0) {
+            return romanNumbers[0];
+        }
+
+        while (number > 0) {
+            if (number >= presentNumbers[iter]) {
+                result += romanNumbers[iter];
+                number -= presentNumbers[iter];
+            } else {
+                iter--;
+            }
+
+        }
+
+        return result;
+    });
+
+    time = hoursAndMinuts.join(':');
+
     return time;
 }
 

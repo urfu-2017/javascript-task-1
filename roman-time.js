754ba6544df1797a29b677901acaccb6a6618ea7
Ilya Kulygin <ruserixsian@gmail.com>
2017-09-29 09:53:16 +0500

    Mini version

--- roman-time.js
+++ roman-time.js
@@ -1,48 +1,17 @@
 'use strict';
 
-const isValidNumber = (value, a, b) => isFinite(value) && value >= a && value <= b;
-
-const parseTime = (time) => {
-    if (typeof time !== 'string' || !time.match(/^\d{2}:\d{2}$/)) {
-        throw new TypeError();
-    }
-
-    const tokens = time.split(':');
-    const hours = parseInt(tokens[0], 10);
-    const minutes = parseInt(tokens[1], 10);
-
-    if (!isValidNumber(hours, 0, 23) || !isValidNumber(minutes, 0, 59)) {
-        throw new TypeError();
-    }
-
-    return { hours, minutes };
-};
-
-const tens = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
-const ones = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
-
-const convertToRoman = (number) => {
-    const indexOfTens = parseInt(number / 10, 10);
-    const indexOfOnes = parseInt(number % 10, 10);
-
-    if (indexOfTens === 0 && indexOfOnes === 0) {
-        return 'N';
-    }
-
-    return tens[indexOfTens] + ones[indexOfOnes];
-};
+const TIME_PATTERN = /^([01]?[0-9]|2[0-3]):([0-5][0-9])$/;
+const ROMAN_TENS = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+const ROMAN_ONES = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
 
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
+ * @throws {TypeError} - если время не корректно (неочевидный текст ошибки)
  */
-function romanTime(time) {
-    const parsedTime = parseTime(time);
-
-    const romanHours = convertToRoman(parsedTime.hours);
-    const romanMinutes = convertToRoman(parsedTime.minutes);
-
-    return `${romanHours}:${romanMinutes}`;
-}
+const romanTime = time => TIME_PATTERN.exec(time)
+    .slice(1, 3)
+    .map(segment => ROMAN_TENS[Math.floor(segment / 10)] + ROMAN_ONES[segment % 10] || 'N')
+    .join(':');
 
 module.exports = romanTime;

81b6d8e8f47b46a5b9678f4bd11f0f6df861f0d6
AlexBarett <iron.angel876527@gmail.com>
2017-10-01 19:58:59 +0500

    вторая попытка

--- roman-time.js
+++ roman-time.js
@@ -4,16 +4,17 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-var ROMAN_MINUTES = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
-var ROMAN_HOURS = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+var ROMAN_ONES = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+var ROMAN_TENS = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
 
 function toRomanNumbers(arabic) {
     var roman = '';
+    arabic = parseInt(arabic);
     if (arabic === 0) {
         roman += 'N';
     } else {
-        roman = roman + ROMAN_MINUTES[(arabic % 10)];
-        roman = ROMAN_HOURS[parseInt (arabic / 10, 10)] + roman;
+        roman = roman + ROMAN_ONES[(arabic % 10)];
+        roman = ROMAN_TENS[parseInt (arabic / 10, 10)] + roman;
     }
 
     return roman;
@@ -22,9 +23,10 @@ function toRomanNumbers(arabic) {
 
 
 function romanTime(time) {
-    var hours = parseInt (time.split(':')[0], 10);
-    var minutes = parseInt (time.split(':')[1], 10);
-    if (isNaN (hours) && isNaN (minutes) && (hours + ':' + minutes) === time) {
+    var hours = time.split(':')[0];
+    var minutes = time.split(':')[1];
+    if (isNaN (hours) && isNaN (minutes) &&
+            parseInt (hours) !== hours && parseInt (minutes) !== minutes) {
         throw new TypeError();
     } else if ((hours < 24 && hours >= 0) && (minutes < 60 && minutes >= 0)) {
         time = toRomanNumbers(hours) + ':' + toRomanNumbers(minutes);

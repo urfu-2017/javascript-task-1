01ee0468a8a76421d63378a6b506d521fc2a3ee8
Maidoo1 <exxelpwnz@mail.ru>
2017-10-01 20:04:27 +0500

    (

--- roman-time.js
+++ roman-time.js
@@ -16,13 +16,14 @@ function parseTime(time, iter) {
 
     if (isNaN(parsedNum)) {
         throw TypeError;
-    } 
+    }
+    
     return parsedNum;
 }
 
 function arabToRoman(tens, units) {
     let romanNums = {
-        0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 
+        0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII',
         9: 'IX', 10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L'
     };
 
@@ -42,19 +43,20 @@ function changeTime(hours, minutes) {
     if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
         throw TypeError;
     }
-    let tensHour = div(hours, 10);    
+    let tensHour = div(hours, 10);
     let unitsHour = hours % 10;
     let tensMinutes = div(minutes, 10);
     let unitsMinutes = minutes % 10;
+    
     return arabToRoman(tensHour, unitsHour) + ':' + arabToRoman(tensMinutes, unitsMinutes);
 }
 
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    try {        
+    try {
         time = time.split(':');
         let hours = parseTime(time, 0);
         let minutes = parseTime(time, 1);
+        
         return changeTime(hours, minutes);
     } catch (err) {
         return 'TypeError: Неверное время';

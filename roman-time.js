102fa5bd7efa0d13f37bbd0b061beda8a71cd43a
Paul Khomich <paulkhomich@outlook.com>
2017-10-01 15:06:35 +0500

    0.0.7

--- roman-time.js
+++ roman-time.js
@@ -10,9 +10,11 @@ function romanTime(time) {
         const minute = Number(time.split(':')[1]);
         if (hour >= 0 && hour < 24 && minute >= 0 && minute < 60) {
             let answer = '';
-            answer += hour === 0 ? 'N' : firstChanger(parseInt(hour / 10)) + secondChanger(hour % 10);
+            answer += hour === 0 ? 'N' : firstChanger(parseInt(hour / 10)) +
+                                            secondChanger(hour % 10);
             answer += ':';
-            answer += minute === 0 ? 'N' : firstChanger(parseInt(minute / 10)) + secondChanger(minute % 10);
+            answer += minute === 0 ? 'N' : firstChanger(parseInt(minute / 10)) +
+                                            secondChanger(minute % 10);
 
             return answer;
         }
@@ -23,49 +25,15 @@ function romanTime(time) {
 }
 
 function firstChanger(numFirst) {
-    switch (numFirst) {
-        case 0:
-            return '';
-        case 1:
-            return 'X';
-        case 2:
-            return 'XX';
-        case 3:
-            return 'XXX';
-        case 4:
-            return 'XL';
-        case 5:
-            return 'L';
-        default:
-            return '';
-    }
+    let romanFirst = ['','X','XX','XXX','XL','L']
+
+    return romanFirst[numFirst];
 }
 
 function secondChanger(numSecond) {
-    switch (numSecond) {
-        case 0:
-            return '';
-        case 1:
-            return 'I';
-        case 2:
-            return 'II';
-        case 3:
-            return 'III';
-        case 4:
-            return 'IV';
-        case 5:
-            return 'V';
-        case 6:
-            return 'VI';
-        case 7:
-            return 'VII';
-        case 8:
-            return 'VIII';
-        case 9:
-            return 'IX';
-        default:
-            return '';
-    }
+    let romanSecond = ['','I','II','III','IV','V','VI','VII','VIII','IX']
+    
+    return romanSecond[numSecond];
 }
 
 module.exports = romanTime;

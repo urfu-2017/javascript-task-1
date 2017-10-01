f6fd3d0e015acbe5e6a80dac6a5b2e3656dc00ee
LevshinMisha <LevshinMisha@gmail.com>
2017-10-01 14:38:25 +0500

    Second commit

--- roman-time.js
+++ roman-time.js
@@ -1,19 +1,19 @@
 'use strict';
 
 function getRomanNumber(number, isFirstNumber) {
-    var romanNumbers = [
+    if (number === 0 && isFirstNumber) {
+        return 'N';
+    }
+    let romanNumbers = [
         { number: 'L', value: 50 }, { number: 'XL', value: 40 },
         { number: 'X', value: 10 }, { number: 'IX', value: 9 },
         { number: 'V', value: 5 }, { number: 'IV', value: 4 },
         { number: 'I', value: 1 }
     ];
-    for (var i = 0; i < romanNumbers.length; i++) {
+    for (let i = 0; i < romanNumbers.length; i++) {
         if (number >= romanNumbers[i].value) {
             return romanNumbers[i].number + getRomanNumber(number - romanNumbers[i].value, false);
         }
-    }
-    if (number === 0 && isFirstNumber) {
-        return 'N';
     }
 
     return '';
@@ -33,18 +33,18 @@ function parseToInt(number) {
 
 
 function romanTime(time) {
-    var splittedTime = time.split(':');
+    let splittedTime = time.split(':');
     if (splittedTime.length !== 2) {
         throw new TypeError();
     }
-    var hours = parseToInt(splittedTime[0]);
-    var minutes = parseToInt(splittedTime[1]);
+    let hours = parseToInt(splittedTime[0]);
+    let minutes = parseToInt(splittedTime[1]);
     if (isNaN(hours) || isNaN(minutes) ||
         !numberIsBetween(hours, 0, 23) || !numberIsBetween(minutes, 0, 59)) {
         throw new TypeError();
     }
 
-    return getRomanNumber(hours) + ':' + getRomanNumber(minutes);
+    return getRomanNumber(hours, true) + ':' + getRomanNumber(minutes, true);
 }
 
 module.exports = romanTime;

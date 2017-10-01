a93872bf45bbaef61779b2212e35142ad17c8e5c
axel86rus <lopuchaleksey@mail.ru>
2017-10-01 17:48:02 +0500

    Исправления 1

--- roman-time.js
+++ roman-time.js
@@ -5,38 +5,39 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 
- var regex = /\d\d:\d\d/;
+var regex = /\d\d:\d\d/;
 
-function isValidation(hour, minute){
+function isValidation(hour, minute) {
     return hour >= 0 && minute >= 0 && hour < 24 && minute < 60;
 }
 
-function numberToriman(hour, minut){
-    var result = "";
+function numberToriman(hour, minut) {
+    var result = '';
     var romanUnits = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
     var romanDozens = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
 
-    res = (hour === 0) ? 'N' : (romanDozens[(hour - hour % 10) / 10] + romanUnits[hour % 10]);
+    result = (hour === 0) ? 'N' : (romanDozens[(hour - hour % 10) / 10] + romanUnits[hour % 10]);
 
-    res += ':';
+    result += ':';
 
-    res += (minut === 0) ? 'N' : (romanDozens[(minut - minut % 10) / 10] + romanUnits[minut % 10]);
+    result += (minut === 0) ? 'N' : (romanDozens[(minut - minut % 10) / 10] + romanUnits[minut % 10]);
 
     return result;
 }
 
 function romanTime(time) {
-    if (typeof time !== "string" || time.length !== 5 || time.match(regex) === null){
-        throw new TypeError("Неверное вреимя");
+    if (typeof time !== 'string' || time.length !== 5 || time.match(regex) === null) {
+        throw new TypeError('Неверное вреимя');
     }
     time = time.split(':');
     var hour = Number(time[0]);
     var minute = Number(time[1]);
-    if (isValidation(hour, minute)){  
+    if (isValidation(hour, minute)) {
         time = numberToriman(hour, minute);
-    }else{
-        throw new TypeError("Неверное время");
+    } else {
+        throw new TypeError('Неверное время');
     }
+    
     return time;
 }
 

f405b4c991a5d99b7a3846988f3113a33ffbc45d
Maxim Akulin <maxim.akulin@gmail.com>
2017-10-01 18:16:27 +0500

    functionality is implemented

--- roman-time.js
+++ roman-time.js
@@ -5,8 +5,38 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    if (!isValidFormat(time))
+        throw new TypeError("invalid time format")
+    var [hours, minutes] = parseTime(time)
+    var romanHours = romanNumber(hours)
+    var romanMinutes = romanNumber(minutes)
+    return `${romanHours}:${romanMinutes}`;
+}
+
+function romanNumber(number) {
+    if (number == 0)
+        return 'N'
+    var digits = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+                  "","I","II","III","IV","V","VI","VII","VIII","IX"]
+    return digits[~~(number / 10)] + digits[10 + number % 10]
+}
+
+function parseTime(time) {
+    var numbers = time.split(':').map(Number)
+    var hours = numbers[0],
+    minutes = numbers[1]
+    return [hours, minutes]
+}
+
+function isValidFormat(time) {
+    if (time == null) 
+        return false;
+    if (typeof(time) !== 'string' && !time instanceof String)
+        return false
+    if (!time.match(/\d\d:\d\d/))
+        return false
+    var [hours, minutes] = parseTime(time)
+    return hours < 24 && minutes < 60
 }
 
 module.exports = romanTime;

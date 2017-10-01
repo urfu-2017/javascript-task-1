d4aa6541fcda84ca7916340bc2a92388ef1e61d6
Paul Khomich <paulkhomich@outlook.com>
2017-10-01 13:32:56 +0500

    0.0.1
    First

--- roman-time.js
+++ roman-time.js
@@ -5,8 +5,81 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    let answerRoman = '';
+    if (time) {
+        const hour = time.split(':')[0];
+        const minute = time.split(':')[1];
+        if (Number(hour) >= 0 && Number(hour) < 24 && Number(minute) >= 0 && Number(minute) < 60) {
+            let hourFirst = hour[0];
+            let hourSecond = hour[1];
+            let minuteFirst = minute[0];
+            let minuteSecond = minute[1];
+            answerRoman = answerRoman + romanChanger(hourFirst,hourSecond) + ':' + romanChanger(minuteFirst,minuteSecond);
+            return answerRoman;
+        } else {
+            throw new TypeError('Error!');
+        }
+    } else {
+        throw new TypeError('Error!');
+    }
+}
+
+function romanChanger(numFirst,numSecond) {
+    if (numFirst == '0' && numSecond == '0') {
+        return 'N';
+    }
+    let returnAns = '';
+    switch (numFirst) {
+        case '0':
+            break;
+        case '1':
+            returnAns = returnAns + 'X';
+            break;
+        case '2':
+            returnAns = returnAns + 'XX';
+            break;
+        case '3':
+            returnAns = returnAns + 'XXX';
+            break;
+        case '4':
+            returnAns = returnAns + 'XL';
+            break;
+        case '5':
+            returnAns = returnAns + 'L';
+            break;
+    }
+    switch (numSecond) {
+        case '0':
+            break;
+        case '1':
+            returnAns = returnAns + 'I';
+            break;
+        case '2':
+            returnAns = returnAns + 'II';
+            break;
+        case '3':
+            returnAns = returnAns + 'III';
+            break;
+        case '4':
+            returnAns = returnAns + 'IV';
+            break;
+        case '5':
+            returnAns = returnAns + 'V';
+            break;
+        case '6':
+            returnAns = returnAns + 'VI';
+            break;
+        case '7':
+            returnAns = returnAns + 'VII';
+            break;
+        case '8':
+            returnAns = returnAns + 'VIII';
+            break;
+        case '9':
+            returnAns = returnAns + 'IX';
+            break;
+    }
+    return returnAns;
 }
 
 module.exports = romanTime;

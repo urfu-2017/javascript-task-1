f9576044c22e290dd92741ac3474234938e6dadc
Asmeev Alexander <daremove1@gmail.com>
2017-10-02 20:27:42 +0500

    Final version

--- roman-time.js
+++ roman-time.js
@@ -4,9 +4,33 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+
+function checkValid(a, b) {
+    return (a < 0 || a > 23 || b < 0 || b > 59 || isNaN(a) || isNaN(b));
+}
+function getRomeTime(a) {
+    var listOfDecimal = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+    var listOfUnit = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+
+    var decimal = Math.floor(a / 10);
+    var unit = a % 10;
+
+    if (decimal === 0 && unit === 0) {
+        return 'N';
+    }
+
+    return listOfDecimal[decimal] + listOfUnit[unit];
+}
+
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    var hours = parseInt(time.split(':')[0], 10);
+    var minutes = parseInt(time.split(':')[1], 10);
+
+    if (checkValid(hours, minutes)) {
+        throw new TypeError('Неверное время');
+    }
+
+    return getRomeTime(hours) + ':' + getRomeTime(minutes);
 }
 
 module.exports = romanTime;

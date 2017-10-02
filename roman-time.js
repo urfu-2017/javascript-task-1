eebc06fe1805c61bb3009891da814c71176d53ab
valerin5 <valera9719@gmail.com>
2017-10-02 23:58:01 +0500

    три

--- roman-time.js
+++ roman-time.js
@@ -14,38 +14,35 @@ function arabToRoman(number) {
     if (number === 0) {
         return 'N';
     }
-   
+
     var ret = '';
     var i = arab.length - 1;
-    while (number > 0) 
-    {
+    while (number > 0) {
         if (number >= arab[i]) {
             ret += roman[i];
             number -= arab[i];
-        }
-        else {
-	        i--;
+        } else {
+            i--;
         }
     }
     return ret;
+
 }
 
 
 function romanTime(time) {
     time = time.split([':']);
-    if (time.length > 2){
+    if (time.length > 2) {
         return TypeError('Неверное время');
+    }
+    if ((time[0] < 24) && (time[1] < 60)) {
+        time[0] = arabToRoman(time[0]);
+        time[1] = arabToRoman(time[1]);
     } else {
-        if ((time[0] < 24) && (time[1] < 60)) {
-            time[0] = arabToRoman(time[0]);
-            time[1] = arabToRoman(time[1]);
-        } else {
-            return TypeError('Неверное время');
-        }
-
+        return TypeError('Неверное время');
     }
     return time.join(':');
-    
+
 }
 
 module.exports = romanTime;

acdfe7681fd787c38234d9f014f601a278839f8b
Xrias <mr.artem.reshetnikov.2017@mail.ru>
2017-10-02 19:02:49 +0500

    Добавлена четвертая версия решения

--- roman-time.js
+++ roman-time.js
@@ -8,23 +8,34 @@ function romanTime(time) {
     var hour = '';
     var minutes = '';
     var hoursMinutes = time.split(':');
-    if (hoursMinutes.length > 2) {
-        throw new TypeError();
-    }
-    if (hoursMinutes[0][0] === '0' && hoursMinutes[0].length < 3) {
-        hoursMinutes[0] = hoursMinutes[0][1];
-    }
-    if (hoursMinutes[1][0] === '0' && hoursMinutes[1].length < 3) {
-        hoursMinutes[1] = hoursMinutes[1][1];
-    }
-    if (isTimeCorrect(Number(hoursMinutes[0]), Number(hoursMinutes[1]))) {
-        hour = arabicToRoman(Number(hoursMinutes[0]));
-        minutes = arabicToRoman(Number(hoursMinutes[1]));
-    } else if (isNaN(hoursMinutes[0]) || isNaN(hoursMinutes[1])) {
+    if (isDataCorrect(hoursMinutes)) {
+        if (hoursMinutes[0][0] === '0') {
+            hoursMinutes[0] = hoursMinutes[0][1];
+        }
+        if (hoursMinutes[1][0] === '0') {
+            hoursMinutes[1] = hoursMinutes[1][1];
+        }
+        if (isTimeCorrect(Number(hoursMinutes[0]), Number(hoursMinutes[1]))) {
+            hour = arabicToRoman(Number(hoursMinutes[0]));
+            minutes = arabicToRoman(Number(hoursMinutes[1]));
+        } else {
+            throw new TypeError();
+        }
+    } else {
         throw new TypeError();
     }
 
     return hour + ':' + minutes;
+}
+
+function isDataCorrect(hoursMinutes) {
+    if (hoursMinutes.length > 2 || hoursMinutes[0].length > 2 || hoursMinutes[1].length > 2 ||
+        isNaN(hoursMinutes[0]) || isNaN(hoursMinutes[1])) {
+
+        return false;
+    }
+
+    return true;
 }
 
 function isTimeCorrect(hours, minutes) {

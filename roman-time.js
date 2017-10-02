c0a08dbba81fafaae86aae6fbf84e466a562fe90
Polina Nebova <nebova@tochka.com>
2017-10-02 13:48:43 +0500

    fix space

--- roman-time.js
+++ roman-time.js
@@ -5,23 +5,23 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 
-function convert(number){
+function convert(number) {
 
-    let arabic_digits = [1, 4, 5, 9, 10, 40, 50];
-    let roman_digits = ['I', 'IV', 'V','IX', 'X', 'XL', 'L'];
+    let arabicDigits = [1, 4, 5, 9, 10, 40, 50];
+    let romanDigits = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
 
-    if (number == 0)
+    if (Number(number) === 0) {
         return 'N';
+    }
 
     let res = '';
-    let idx = arabic_digits.length - 1;
-    
-    while (idx >= 0){
-        while (number >= arabic_digits[idx]){
-            number -= arabic_digits[idx];
-            res += roman_digits[idx];
+    let idx = arabicDigits.length - 1;
+    while (idx >= 0) {
+        while (number >= arabicDigits[idx]) {
+            number -= arabicDigits[idx];
+            res += romanDigits[idx];
         }
-        --idx;   
+        --idx;
     }
 
     return res;
@@ -29,22 +29,25 @@ function convert(number){
 
 function romanTime(time) {
 
-    if (time == null || undefined)
+    if (time === null || undefined) {
         throw new TypeError('incorrect value');
+    }
 
     let pattern = /^(\d{1,2}):(\d{2})$/;
-    let time_pars = time.match(pattern);   
+    let timePars = time.match(pattern);
 
-    if(!time_pars)
+    if (!timePars) {
         throw new TypeError('incorrect value');
+    }
 
-    let hours = time_pars[1];
-    let minutes = time_pars[2];
-    
-    if (hours > 23 || minutes > 59)
+    let hours = timePars[1];
+    let minutes = timePars[2];
+
+    if (hours > 23 || minutes > 59) {
         throw new TypeError('incorrect value');
+    }
 
     return convert(hours) + ':' + convert(minutes);
 }
- 
+
 module.exports = romanTime;

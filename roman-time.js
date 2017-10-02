cd6a0cad7f068c84639170ea914f7828ed8c9f52
Чайников Константин <32215785+Kotyan7@users.noreply.github.com>
2017-10-02 17:48:12 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -4,24 +4,25 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function romanTime(time)
-{   var p; 
+function romanTime(time)   
+   {var p; 
     p=time.length;
-    if (p==5 && time.indexOf(':')===2)
+    if (p===5 && time.indexOf(':')===2)
     {
         time = time.split(":", 2);
-        var Hour, min;
+        var Hour;
+        var min;
         Hour = Number(time[0]);
         min = Number(time[1]);
         if (Hour!==Number(time[0]) || min!==Number(time[1]) || min>=60 || Hour>=24 )
             { throw new TypeError('Неверные данные'); }
         else {
-            var Rim = new Array( 'N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI',
-            'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXIII', 'XXIV',
-            'XXV', 'XXVI', 'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII', 'XXXIII', 'XXXIV', 'XXXV',
-            'XXXVI', 'XXXVII', 'XXXVIII', 'XXXIX', 'XL', 'XLI', 'XLII', 'XLIII', 'XLIV', 'XLV', 'XLVI',
-            'XLVII', 'XLVIII', 'XLIX', 'L', 'LI', 'LII', 'LIII', 'LIV', 'LV', 'LVI', 'LVII',
-            'LVIII', 'LVIX' )
+            var Rim = new Array( 'N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X',
+            'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII',
+            'XXIII', 'XXIV', 'XXV', 'XXVI', 'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII',
+            'XXXIII', 'XXXIV', 'XXXV', 'XXXVI', 'XXXVII', 'XXXVIII', 'XXXIX', 'XL', 'XLI', 'XLII',
+            'XLIII', 'XLIV', 'XLV', 'XLVI', 'XLVII', 'XLVIII', 'XLIX', 'L', 'LI', 'LII', 'LIII',
+            'LIV', 'LV', 'LVI', 'LVII', 'LVIII', 'LVIX' )
             Hour = Rim[Hour];
             min = Rim[min];
             time = Hour + ':' + min;

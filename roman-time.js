7232c1aeb59f70c83f990a075757965bbc11f5d1
Slava Vikharev <slavavikharev@yandex-team.ru>
2017-10-02 23:53:07 +0500

    Задача «XXL»

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,24 @@
 'use strict';
 
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
-function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
-}
+const TIME_REGEX = /^([10][0-9]|2[0-3]):([0-5][0-9])$/;
+const ROMAN_NUMS = { 0: 'N', 1: 'I', 4: 'IV', 5: 'V', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L' };
+
+const toRoman = (number) => {
+    let closest = Math.max(...Object.keys(ROMAN_NUMS).filter(x => x < number));
+
+    return ROMAN_NUMS[number] || ROMAN_NUMS[closest].concat(toRoman(number - closest));
+};
+
+const romanTime = (time) => {
+    if (!TIME_REGEX.test(time)) {
+        throw new TypeError('Incorrect time format');
+    }
+
+    return time
+        .split(':')
+        .map(Number)
+        .map(toRoman)
+        .join(':');
+};
 
 module.exports = romanTime;

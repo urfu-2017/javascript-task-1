a6b55f9fdea5004199501af92f452bae78f4f424
valerin5 <valera9719@gmail.com>
2017-10-02 23:27:03 +0500

    Попытка раз

--- roman-time.js
+++ roman-time.js
@@ -4,9 +4,47 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+var arab = [1, 4, 5, 9, 10, 40, 50];
+var roman = ['I','IV','V','IX','X','XL','L'];
+
+function arabToRoman(number)
+{
+    if(isNaN(number)) return TypeError('Неверное время');
+    if (number == 0) return 'N';
+
+	var ret = '';
+	var i = arab.length - 1;
+	while(number > 0)
+	{
+		if(number >= arab[i])
+		{
+			ret += roman[i];
+			number -= arab[i];
+		}
+		else
+		{
+			i--;
+		}
+	
+	}
+	return ret;
+}
+
+
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    time = time.split([':']);
+    if (time.length > 2){
+        return TypeError('Неверное время');
+    } else {
+        if ((time[0] < 24) && (time[1] < 60)) {
+            time[0] = arabToRoman(time[0]);
+            time[1] = arabToRoman(time[1]);
+        } else {
+            return TypeError('Неверное время');
+        }
+
+    }
+    return time.join(':');
 }
 
 module.exports = romanTime;

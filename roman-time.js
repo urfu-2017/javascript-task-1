d189d71ba139d35154e00c64a57aa50ab2e763b1
iriskis <irinka199701@mail.ru>
2017-10-03 03:45:39 +0500

    A bit of author's code and wonderful magic

--- roman-time.js
+++ roman-time.js
@@ -4,9 +4,28 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+
+const DOZENS = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+const UNITS = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    var correctTime = /^([0-1][0-9]|[2][0-3]):([0-5][0-9])$/;
+    if (!correctTime.test(time)) {
+        throw new TypeError('Invalid time');
+    }
+    var parseTime = time.match(correctTime);
+    var hours = parseInt(parseTime[1], 10);
+    var minuts = parseInt(parseTime[2], 10);
+
+    return toRoman(hours) + ':' + toRoman(minuts);
+}
+
+function toRoman(digit) {
+    if (digit === 0) {
+        return 'N';
+    }
+
+    return DOZENS[Math.floor(digit / 10)] + UNITS[digit % 10];
 }
 
 module.exports = romanTime;

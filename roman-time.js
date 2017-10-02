fd508753d507903950f0c0814a24a1fe45a1ed96
Вичкунина Дарья <vichkunina@bk.ru>
2017-10-02 23:17:20 +0500

    Доша пытается оформить

--- roman-time.js
+++ roman-time.js
@@ -1,36 +1,40 @@
 'use strict';
-function Roman(time) 
-{
+function roman(time) {
     var arab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50];
-    var roman = ['I', 'II', 'III',  'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XX', 'XL', 'XXX', 'XL'];
+    var roma = [
+        'I', 'II', 'III', 'IV', 
+        'V', 'VI', 'VII', 'VIII', 'IX', 'X', 
+        'XX', 'XL', 'XXX', 'XL'];
     var i = arab.length - 1;
+
     var result = '';
-    if (time == 0) 
-    {
+
+    if (time === 0) {
         result = 'N';
     }
-    do
-    {
-        if (time >= arab[i]) 
-        {
-            result += roman[i];
+
+    do {
+        if (time >= arab[i]) {
+            result += roma[i];
             time -= arab[i];
         } 
-        i--;
-        
+        i--;   
     }
-    while (time > 0 && i > 0) 
+
+    while (time > 0 && i > 0); 
+    
     return result;
+
 }
 
-function romanTime(time) 
-{
+function romanTime(time) {
     var time12 = time.split(':');
-    if (parseInt(time12[0], 10).isNaN || parseInt(time12[1], 10).isNaN || parseInt(time12[0], 10) > 23 || parseInt(time12[1], 10) > 59 ||parseInt(time12[0], 10) < 0 || parseInt(time12[1], 10) < 0 || time.length > 5 )
-    {
+    if (parseInt(time12[0], 10).isNaN || parseInt(time12[1], 10).isNaN || parseInt(time12[0], 10) > 23 || parseInt(time12[1], 10) > 59 || time.length > 5 ) {
         throw new TypeError('Неверное время');
     }
-    return(Roman(parseInt(time12[0], 10)) + ':' + Roman(parseInt(time12[1], 10)))
-}
+
+    return(roman(parseInt(time12[0], 10)) + ':' + roman(parseInt(time12[1], 10)));
+
+    }
 
 module.exports = romanTime;

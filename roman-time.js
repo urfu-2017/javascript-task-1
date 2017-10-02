fa8b46f81dcc550366c456c728c770818c090490
Чайников Константин <32215785+Kotyan7@users.noreply.github.com>
2017-10-02 18:26:46 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -4,21 +4,17 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function romanTime(time)
-   {
-    if (time.length === 5 && time.indexOf(':') === 2)
-    {
-        time = time.split(":", 2);
+function romanTime(time) {
+    if (time.length === 5 && time.indexOf(':') === 2) {
+        time = time.split(':', 2);
         var Hour;
         var min;
         Hour = Number(time[0]);
         min = Number(time[1]);
-        if (Hour !== Number(time[0]) || min !== Number(time[1]) || min >= 60 || Hour >= 24 )
-            {
-                throw new TypeError('Неверные данные');
-            }
-        else
-        {
+        if (Hour !== Number(time[0]) || min !== Number(time[1]) || min >= 60 || Hour >= 24 ) {
+            throw new TypeError('Неверные данные');
+        }
+        else {
             var Rim = new Array('N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X',
             'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII',
             'XXIII', 'XXIV', 'XXV', 'XXVI', 'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII',
@@ -31,8 +27,7 @@ function romanTime(time)
             return time;
         }
     }
-    else
-    {
+    else {
         throw new TypeError('Неверные данные');
     }
 }

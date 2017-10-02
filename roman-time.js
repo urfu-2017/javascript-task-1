34b9204f49f0c933b63be724d1ab651e5d33423e
Вичкунина Дарья <vichkunina@bk.ru>
2017-10-02 23:31:38 +0500

    Все еще пытаюсь

--- roman-time.js
+++ roman-time.js
@@ -2,8 +2,8 @@
 function roman(time) {
     var arab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50];
     var roma = [
-        'I', 'II', 'III', 'IV', 
-        'V', 'VI', 'VII', 'VIII', 'IX', 'X', 
+        'I', 'II', 'III', 'IV',
+        'V', 'VI', 'VII', 'VIII', 'IX', 'X',
         'XX', 'XL', 'XXX', 'XL'];
     var i = arab.length - 1;
 
@@ -17,24 +17,25 @@ function roman(time) {
         if (time >= arab[i]) {
             result += roma[i];
             time -= arab[i];
-        } 
-        i--;   
+        }
+        i--;
     }
 
-    while (time > 0 && i > 0); 
-    
+    while (time > 0 && i > 0);
+
     return result;
 
 }
 
 function romanTime(time) {
     var time12 = time.split(':');
-    if (parseInt(time12[0], 10).isNaN || parseInt(time12[1], 10).isNaN || parseInt(time12[0], 10) > 23 || parseInt(time12[1], 10) > 59 || time.length > 5 ) {
+    if (parseInt(time12[0], 10).isNaN || parseInt(time12[1], 10).isNaN
+    || parseInt(time12[0], 10) > 23 || parseInt(time12[1], 10) > 59
+    || time.length > 5 ) {
         throw new TypeError('Неверное время');
     }
 
-    return(roman(parseInt(time12[0], 10)) + ':' + roman(parseInt(time12[1], 10)));
-
-    }
+    return (roman(parseInt(time12[0], 10)) + ':' + roman(parseInt(time12[1], 10)));
+}
 
 module.exports = romanTime;

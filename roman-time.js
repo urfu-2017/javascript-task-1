d8e1eb0eee7a7753b24f732f5193b6a35fafb78a
Чайников Константин <32215785+Kotyan7@users.noreply.github.com>
2017-10-02 18:01:29 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -6,7 +6,7 @@
  */
 function romanTime(time)   
    {var p; 
-    p=time.length;
+    p = time.length;
     if (p===5 && time.indexOf(':')===2)
     {
         time = time.split(":", 2);
@@ -15,21 +15,26 @@ function romanTime(time)
         Hour = Number(time[0]);
         min = Number(time[1]);
         if (Hour!==Number(time[0]) || min!==Number(time[1]) || min>=60 || Hour>=24 )
-            { throw new TypeError('Неверные данные'); }
-        else {
-            var Rim = new Array( 'N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X',
+            {
+                throw new TypeError('Неверные данные');
+            }
+        else
+        {
+            var Rim = new Array['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X',
             'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII',
             'XXIII', 'XXIV', 'XXV', 'XXVI', 'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII',
             'XXXIII', 'XXXIV', 'XXXV', 'XXXVI', 'XXXVII', 'XXXVIII', 'XXXIX', 'XL', 'XLI', 'XLII',
             'XLIII', 'XLIV', 'XLV', 'XLVI', 'XLVII', 'XLVIII', 'XLIX', 'L', 'LI', 'LII', 'LIII',
-            'LIV', 'LV', 'LVI', 'LVII', 'LVIII', 'LVIX' )
+            'LIV', 'LV', 'LVI', 'LVII', 'LVIII', 'LVIX'];
             Hour = Rim[Hour];
             min = Rim[min];
             time = Hour + ':' + min;
-            return time; }
-            }
-    else {
-        throw new TypeError('Неверные данные') ;
+            return time;
         }
+    }
+    else 
+    {
+        throw new TypeError('Неверные данные');
+    }
 }
 module.exports = romanTime;

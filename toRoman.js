c6fa3115a62aee141a47d4f7dc2371cd831dfe16
Saypiev Anvar <who-e@yandex.ru>
2017-10-04 04:23:55 +0500

    Rewrite toRoman

--- toRoman.js
+++ toRoman.js
@@ -8,17 +8,20 @@ module.exports = function (num) {
     if (num === 0) {
         return 'N';
     }
+    var roman = ['L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+    var arabic = [50, 40, 10, 9, 5, 4, 1];
     var result = '';
-    var l = Math.floor(num / 50);
-    result += new Array(l + 1).join('L');
-    num %= 50;
-    var x = Math.floor(num / 10);
-    num %= 10;
-    result = x > 3 ? 'X' + result : result;
-    var firstTen = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
-    result += num > 8 ? firstTen[num - 1] : '';
-    result += new Array(x + 1).join('X');
-    result += num <= 8 && num > 0 ? firstTen[num - 1] : '';
+    var i = 0;
+    while (i < 7) {
+        var value = arabic[i];
+        var symbol = roman[i];
+        if (value <= num) {
+            result += symbol;
+            num -= value;
+        } else {
+            i++;
+        }
+    }
 
     return result;
 };

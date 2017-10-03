f31fa7f1a7e3db1f463f6b2a4e757480abaf7116
chrighter <christina.ilyenko@yandex.ru>
2017-10-03 17:25:31 +0500

    refactoring

--- roman-time.js
+++ roman-time.js
@@ -1,31 +1,26 @@
 'use strict';
 
-/**
- * @param {String} time – время в формате HH:MM (например, 09:05)
- * @returns {String} – время римскими цифрами (IX:V)
- */
 var ARABIC = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60];
-var ROMAN = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII',
-        'IX', 'X', 'XX', 'XXX', 'XL', 'L', 'LX'];
-var ROMANHOUR = '';
-var ROMANMINUTE = '';
-var ROMANNUMBER = '';
+var ROMAN = [
+    'N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII',
+    'IX', 'X', 'XX', 'XXX', 'XL', 'L', 'LX'
+];
 
 function translate(number) {
+    var romanNumber = '';
     if (number === 0) {
-        ROMANNUMBER += 'N';
+        romanNumber += 'N';
 
-        return ROMANNUMBER;
+        return romanNumber;
     }
 
-    ROMANNUMBER += ROMAN[findLeftBorder(number)[1]];
-    number -= findLeftBorder(number)[0];
-    if (number !== 0) {
-        translate(number);
+    romanNumber += ROMAN[findLeftBorder(number)[1]];
+    var remainder = number - findLeftBorder(number)[0];
+    if (remainder !== 0) {
+        romanNumber += ROMAN[remainder];
     }
 
-    return ROMANNUMBER;
-
+    return romanNumber;
 }
 
 function findLeftBorder(number) {
@@ -40,26 +35,25 @@ function findLeftBorder(number) {
     }
 }
 
-function checkInputCorrect(time) {
-    if (!(/^[0-1][0-9]|[2][2-3]:[0-5][0-9]$/.test(time))) {
+function assertInputCorrect(time) {
+    if (!/^[0-1][0-9]|[2][2-3]:[0-5][0-9]$/.test(time)) {
         throw new TypeError('Неверное время');
     }
-
-    return true;
 }
 
+/**
+ * @param {String} time – время в формате HH:MM (например, 09:05)
+ * @returns {String} – время римскими цифрами (IX:V)
+ */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии 0:0
-    if (checkInputCorrect(time)) {
-        var timeParts = time.split(':');
-        var hours = Number(timeParts[0]);
-        var minutes = Number(timeParts[1]);
-        ROMANHOUR = translate(hours);
-        ROMANNUMBER = '';
-        ROMANMINUTE = translate(minutes);
-        ROMANNUMBER = '';
+    assertInputCorrect(time);
+    var timeParts = time.split(':');
+    var hours = Number(timeParts[0]);
+    var minutes = Number(timeParts[1]);
+    var romanHour = translate(hours);
+    var romanMinute = translate(minutes);
 
-        return ROMANHOUR + ':' + ROMANMINUTE;
-    }
+    return romanHour + ':' + romanMinute;
 }
 module.exports = romanTime;

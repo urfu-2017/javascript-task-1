e4f4ec0396c561d69247b45853f012f82d75501c
eimic <korabelnikovat@gmail.com>
2017-10-03 10:41:17 +0500

    solution

--- roman-time.js
+++ roman-time.js
@@ -6,7 +6,45 @@
  */
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
-    return time;
+    if (!time) {
+        throw new TypeError('Неверное время');
+    }
+    let [hours, minutes] = time.split(':', 2);
+    hours = parseInt(hours, 10);
+    minutes = parseInt(minutes, 10);
+    if (isNaN(hours) || isNaN(minutes) || !isValid(hours, minutes)) {
+        throw new TypeError('Неверное время');
+    }
+
+    return convertToRoman(hours) + ':' + convertToRoman(minutes);
+}
+
+function isValid(hours, minutes) {
+    if (hours >= 24 || hours < 0 || minutes > 59 || minutes < 0) {
+        return false;
+    }
+
+    return true;
+}
+
+function convertToRoman(number) {
+    let arab = [0, 1, 4, 5, 9, 10, 40, 50];
+    let roman = ['N', 'I', 'IV', 'V', 'IX', 'X', 'XL', 'L'];
+    let i = arab.length;
+    let result = '';
+    if (number === 0) {
+        return 'N';
+    }
+    while (number > 0) {
+        if (number >= arab[i]) {
+            result += roman[i];
+            number -= arab[i];
+        } else {
+            --i;
+        }
+    }
+
+    return result;
 }
 
 module.exports = romanTime;

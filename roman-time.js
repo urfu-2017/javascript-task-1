c33273054d587bfd4e8371c5b47635c16390663c
King-Of-Plague1 <korob228@bk.ru>
2017-10-03 15:18:23 +0500

    Reincarnation

--- roman-time.js
+++ roman-time.js
@@ -5,47 +5,21 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    var numbersNorm = ['1',
-                       '2',
-                       '3',
-                       '4',
-                       '5',
-                       '6',
-                       '7',
-                       '8',
-                       '9',
-                       '0'];
 
-    var numbersRome = ['I',
-                       'II',
-                       'III',
-                       'IV',
-                       'V',
-                       'VI',
-                       'VII',
-                       'VIII',
-                       'IX',
-                       'N'];
-
-    var numbersDecadeR = ['X',
-                          'XX',
-                          'XXX',
-                          'XL',
-                          'L'];
-
+    var timeNorm;
     var timeRome;
+    var answer = [];
     if (isTimeCorrect(time)) {
-        var timeNorm = time.split(':');
-        timeRome = transmutation(numbersDecadeR, timeNorm, numbersNorm, numbersRome);
-        delete timeRome[timeRome.length - 1];
-        time = timeRome.join('');
-    }
-
-    else {
+        timeNorm = time.split(':');
+        timeNorm = timeNorm.join('');
+        timeNorm = timeNorm.slice();
+        timeRome = transmutation(timeNorm);
+        answer = correction(timeRome);
+        time = answer.join('');
+    } else {
         time = 'TypeError: Неверное время';
     }
 
-// Немного авторского кода и замечательной магии
 
     return time;
 }
@@ -53,52 +27,73 @@ function romanTime(time) {
 module.exports = romanTime;
 
 function isTimeCorrect(time) {
-
-    var timeCorrect = true;
-
     try {
         var timeNorm = time.split(':');
-
-        if (timeNorm[0] >= 24 || timeNorm[1] > 59) {
-            timeCorrect = false;
+        var isCorrect = true;
+        if (timeNorm[0] < 0 || timeNorm[1] < 0) {
+            isCorrect = false;
         }
-        if (timeNorm[0] < 0 || timeNorm[1] < 0 || time.length !== 5) {
-            timeCorrect = false;
+        if (timeNorm[0] > 23 || timeNorm[1] > 59) {
+            isCorrect = false;
         }
+
+        return isCorrect;
+    } catch (err) {
+        var isntCorrect = false;
+
+        return isntCorrect;
     }
-
-    catch (err) {
-        timeCorrect = false;
-    }
-
-    return timeCorrect;
-
 }
 
-function transmutation(numbersDecadeR, timeNorm, numbersNorm, numbersRome) {
+function transmutation(timeNorm) {
+
+    var numbersRome = ['N',
+                        'I',
+                        'II',
+                        'III',
+                        'IV',
+                        'V',
+                        'VI',
+                        'VII',
+                        'VIII',
+                        'IX',
+                        'N',
+                        'X',
+                        'XX',
+                        'XXX',
+                        'XL',
+                        'L'];
     var timeRome = [];
-    for (var i = 0; i < timeNorm.length; i ++) {
-        var excessN = 0;
-        for (let j = 0; j < numbersDecadeR.length; j++) {
-            if (String(timeNorm[i])[0] === numbersNorm[j]) {
-                excessN++;
-                timeRome.push(numbersDecadeR[j]);
-            }        
+    for (let i = 0; i < timeNorm.length; i++) {
+        var exN = 0;
+        if (i % 2 === 0) {
+            timeRome.push(numbersRome[parseInt(timeNorm[i]) + 10]);
+
+        } else {
+            timeRome.push(numbersRome[timeNorm[i]]);
+            exN ++;
         }
-        for (let j = 0; j < numbersNorm.length; j++) {
-            if (String(timeNorm[i])[1] === numbersNorm[j]){
-                timeRome.push(numbersRome[j]);
-            }     
-        }
-        for (var k = 0; k < excessN; k++) {
+
+        for (let k = 0; k < exN; k++) {
             delete timeRome[timeRome.indexOf('N')];
         }
-
-        timeRome.push(':');
-
     }
 
     return timeRome;
 }
 
+function correction(timeRome) {
+    timeRome = timeRome.join(' ');
+    timeRome = timeRome.replace(/\s+/g, ' ');
+    timeRome = timeRome.replace(/^\s*/, '').replace(/\s*$/, '');
+    timeRome = timeRome.split(' ');
+    var answer = [];
+    for (let i = 0; i < timeRome.length; i++) {
+        answer.push(timeRome[i]);
+        if (i === (timeRome.length / 2) - 1) {
+            answer.push(':');
+        }
+    }
 
+    return answer;
+}

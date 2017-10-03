7597a82a41b145051a61a636f2d687afc6f0d982
Nikolai Borodin <meta1-heart@yandex.ru>
2017-10-04 03:03:21 +0500

    #finale 2 try

--- roman-time.js
+++ roman-time.js
@@ -11,64 +11,59 @@ function hoursToRoman(time) {
     var h1 = parseInt(HH[0]); // разбиваем поэлементно часы, преобразуя к Int
     var h2 = parseInt(HH[1]);
     let dicH1 = {
-        '0' : '',
-        '1' : 'X',
-        '2' : 'XX'
+        '0': '',
+        '1': 'X',
+        '2': 'XX'
     };
     let dicH2 = {
-        '0' : '',
-        '1' : 'I',
-        '2' : 'II',
-        '3' : 'III',
-        '4' : 'IV',
-        '5' : 'V',
-        '6' : 'VI',
-        '7' : 'VII',
-        '8' : 'VIII',
-        '9' : 'IX'
+        '0': '',
+        '1': 'I',
+        '2': 'II',
+        '3': 'III',
+        '4': 'IV',
+        '5': 'V',
+        '6': 'VI',
+        '7': 'VII',
+        '8': 'VIII',
+        '9': 'IX'
     };
-    
     if ((h1 === 0) && (h2 === 0)) {
         
-        return 'N';
+    return 'N';
     }
 
     return dicH1[h1] + dicH2[h2];
  }
-
-function minutesToRoman(time){
+function minutesToRoman(time) {
     // перевод минут
     var arrayOfTime = time.split(':'); // Делим входное время на две части
     var MM = arrayOfTime[1]; // Минуты
     var m1 = parseInt(MM[0]); // разбиваем поэлементно минуты, преобразуя к Int
     var m2 = parseInt(MM[1]); // ---
-    var rm1 = '';
-    var rm2 = '';
     let dicM1 = {
-        '0' : '',
-        '1' : 'X',
-        '2' : 'XX',
-        '3' : 'XX',
-        '4' : 'XX',
-        '5' : 'XX'
+        '0': '',
+        '1': 'X',
+        '2': 'XX',
+        '3': 'XX',
+        '4': 'XX',
+        '5': 'XX'
     };
     let dicM2 = {
-        '0' : '',
-        '1' : 'I',
-        '2' : 'II',
-        '3' : 'III',
-        '4' : 'IV',
-        '5' : 'V',
-        '6' : 'VI',
-        '7' : 'VII',
-        '8' : 'VIII',
-        '9' : 'IX'
+        '0': '',
+        '1': 'I',
+        '2': 'II',
+        '3': 'III',
+        '4': 'IV',
+        '5': 'V',
+        '6': 'VI',
+        '7': 'VII',
+        '8': 'VIII',
+        '9': 'IX'
     };
     if ((m1 === 0) && (m2 === 0)) {
 
-        return 'N';
+    return 'N';
     }
-
     return dicM1[m1] + dicM2[m2];
  } 
 function romanTime(time) {

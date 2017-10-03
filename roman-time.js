d6465b973b0fa5e64530af7ec843757c8818a5aa
AZ404 <ztbzak1@rambler.ru>
2017-10-04 00:23:01 +0500

    task done

--- roman-time.js
+++ roman-time.js
@@ -5,8 +5,60 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    var romanValues = parseTime(time).map(intToRoman);
+
+    return romanValues[0] + ':' + romanValues[1];
+}
+
+function parseTime(time) {
+    if (typeof time !== 'string') {
+        throw new TypeError();
+    }
+    var tokens = time.split(':');
+    var hours = Number(tokens[0]);
+    var minutes = Number(tokens[1]);
+    if (isNaN(hours) || isNaN(minutes) || !inRange(0, hours, 23) || !inRange(0, minutes, 59)) {
+        throw new TypeError();
+    }
+
+    return [hours, minutes];
+}
+
+function intToRoman(value) {
+    var rules = {
+        1: 'I',
+        4: 'IV',
+        5: 'V',
+        9: 'IX',
+        10: 'X',
+        40: 'XL',
+        50: 'L'
+    };
+    if (value === 0) {
+        return 'N';
+    }
+    var result = '';
+    var arabicValues = Object.keys(rules).sort((a, b) => (a - b))
+        .reverse();
+    for (var i = 0; i < arabicValues.length; i++) {
+        var arabicValue = arabicValues[i];
+        if (value >= arabicValue) {
+            value -= arabicValue;
+            result += rules[arabicValue];
+            break;
+        }
+    }
+
+    if (value > 0) {
+        result += intToRoman(value);
+    }
+
+    return result;
+}
+
+function inRange(lowerBound, value, upperBound) {
+
+    return lowerBound <= value && value <= upperBound;
 }
 
 module.exports = romanTime;

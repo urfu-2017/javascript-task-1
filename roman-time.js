8a6b687135415bebc22cae4e589b42545d2a025e
alex43jk <leonbkone@yandex.ru>
2017-10-04 00:08:38 +0500

    Strings must use singlequote

--- roman-time.js
+++ roman-time.js
@@ -5,25 +5,26 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    if (time[2] != ":") {
+    if (time[2] !== ':') {
         throw new TypeError("Неверное время");
     }
-    var arr = time.split(":");
+    var arr = time.split(':');
     var hour = arr[0];
     var minute = arr[1];
     
     hour = toHour(hour);
     minute = toMinute(minute);
-    time = hour + ":" + minute;
+    
+    time = hour + ':' + minute;
     return time;
 }
 
 function toHour(arr) {
-    var hours = ["", "X", "XX"];
-    var minutes = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-    var hour = "N";
+    var hours = ['', 'X', 'XX'];
+    var minutes = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    var hour = 'N';
     if (arr.length == 2 && Number(arr) >= 0 && Number(arr) < 24) {
-        var num = arr.split("");
+        var num = arr.split('');
         if (Number(num[0]) == 0 && Number(num[1]) == 0) {
             return hour;
         }
@@ -35,22 +36,22 @@ function toHour(arr) {
                         num[1] = minutes[j];
                     }
                 }
-                hour = num.join("");
+                hour = num.join('');
                 return hour;
             }
         }
         
     }
-    throw new TypeError("Неверное время");
+    throw new TypeError('Неверное время');
 }
 
 function toMinute(arr) {
-    var hours = ["", "X", "XX", "XXX", "XL", "L"];
-    var minutes = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
-    var minute = "N";
+    var hours = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+    var minutes = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    var minute = 'N';
 
     if (arr.length == 2 && Number(arr) >= 0 && Number(arr) < 60) {
-        var num = arr.split("");
+        var num = arr.split('');
         if (Number(num[0]) == 0 && Number(num[1]) == 0) {
             return minute;
         }
@@ -62,12 +63,12 @@ function toMinute(arr) {
                         num[1] = minutes[j];
                     }
                 }
-                minute = num.join("");
+                minute = num.join('');
                 return minute;
             }
         }
     }
-    throw new TypeError("Неверное время");
+    throw new TypeError('Неверное время');
 }
 
 module.exports = romanTime;

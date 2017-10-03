e4034186a5b9616c60541cfde50b8edd57658fcb
Naturin <bontortue@gmail.com>
2017-10-04 01:16:46 +0500

    step 2

--- roman-time.js
+++ roman-time.js
@@ -8,128 +8,77 @@ function romanTime(time) {
     var arr = time.split(':');
     var hourArab = parseInt(arr[0]);
     var minuteArab = parseInt(arr[1]);
-    var ten = 10;
-    var five = 5;
-    var one = 1;
-    var fifty = 50;
-    var fourty = 40;
-    var thirty = 30;
-    var twenty = 20;
-    var time;
-
-    var hourRim, minuteRim, remainH, remainM;
     if (isNaN(hourArab) || isNaN(minuteArab) || hourArab >= 24 || hourArab < 0 || minuteArab < 0 || minuteArab > 60 ) {
         throw new TypeError;
     }
-    if (Math.floor(minuteArab/fifty) === 1)
+    function toRoman(number)
     {
-        minuteRim = 'L';
-        remainM = minuteArab % fifty;
-    }
-    if (Math.floor(minuteArab/ten) === 4)
-    {
-        minuteRim = 'XL';
-        remainM = minuteArab % fourty;
-    }
-    if (Math.floor(minuteArab/ten) === 3)
-    {
-        minuteRim = 'XXX';
-        remainM = minuteArab % thirty;
-    }
-    if (Math.floor(minuteArab/ten) === 2)
-    {
-        minuteRim = 'XX';
-        remainM = minuteArab % twenty;
-    }
-    if (Math.floor(minuteArab/ten) === 1)
-    {
-        minuteRim = 'X';
-        remainM = minuteArab % ten;
-    }
-    if (Math.floor(minuteArab/ten) === 0)
-    {
-        remainM = minuteArab;
-    }
-    if (remainM > 5 && remainM < 9)
-    {
-        while (remainM > 5)
+        var rimNumber = '';
+        var remain;
+        if (Math.floor(number/50) === 1)
         {
-            minuteRim  = minuteRim + 'I';
-            remainM--;
+            rimNumber = 'L';
+            remain = number % 50;
         }
-    }
-    if (remainM === 9)
-    {
-        minuteRim = minuteRim + 'IX';
-    }
-    if (remainM < 4)
-    {
-        while (remainM !== 0)
+        if (Math.floor(number/10) === 4)
         {
-            minuteRim = minuteRim + 'I';
-            remainM--;
+            rimNumber = 'XL';
+            remain = number % 40;
         }
-    }
-    if (remainM === 4)
-    {
-        minuteRim = minuteRim + 'IV';
-    }
-    if (remainM === 5)
-    {
-        minuteRim = minuteRim + 'V';
-    }
-    if (Math.floor(hourArab/ten) === 2)
-    {
-        hourRim = 'XX';
-        remainH = hourArab % twenty;
-    }
-    if (Math.floor(minuteArab/ten) === 1)
-    {
-        hourRim = 'X';
-        remainH = hourArab % ten;
-    }
-    if (Math.floor(hourArab/ten) === 0)
-    {
-        remainH = hourArab;
-    }
-    if (remainH > 5 && remainH < 9)
-    {
-        while (remainH > 5)
+        if (Math.floor(number/10) === 3)
         {
-            hourRim  = hourRim + 'I';
-            remainH--;
+            rimNumber = 'XXX';
+            remain = number % 30;
         }
-    }
-    if (remainH === 9)
-    {
-        hourRim = hourRim + 'IX';
-    }
-    if (remainH < 4)
-    {
-        while (remainH !== 0)
+        if (Math.floor(number/10) === 2)
         {
-            hourRim = hourRim + 'I';
-            remainH--;
+            rimNumber = 'XX';
+            remain = number % 20;
         }
+        if (Math.floor(number/10) === 1)
+        {
+            rimNumber = 'X';
+            remain = number % 10;
+        }
+        if (Math.floor(number/10) === 0)
+        {
+            remain = number;
+        }
+        if (remain > 5 && remain < 9)
+        {
+            while (remain > 5)
+            {
+                rimNumber += 'I';
+                remain--;
+            }
+        }
+        if (remain === 9)
+        {
+            rimNumber += 'IX';
+        }
+        if (remain < 4)
+        {
+            while (remain !== 0)
+            {
+                rimNumber += 'I';
+                remain--;
+            }
+        }
+        if (remain === 4)
+        {
+            rimNumber += 'IV';
+        }
+        if (remain === 5)
+        {
+            rimNumber += 'V';
+        }
+        if (Math.floor(number/1) === 0)
+        {
+            rimNumber = 'NN';
+        }
+        return rimNumber;
     }
-    if (remainH === 4)
-    {
-        hourRim = hourRim + 'IV';
-    }
-    if (remainH === 5)
-    {
-        hourRim = hourRim + 'V';
-    }
-    if (Math.floor(hourArab/one) === 0)
-    {
-        hourRim = 'NN';
-    }
-    if (Math.floor(minuteArab/one) === 0)
-    {
-        minuteRim = 'NN';
-    }
-    time = hourRim +  ':' + minuteRim;
-    return time;
+    return toRoman(hourArab) +  ':' + toRoman(minuteArab);
 }
 
 module.exports = romanTime;

22f0a221dc2d6b92cf84829e514d99726f012bdd
tatiannesterova <tatiannesterova@gmail.com>
2017-10-04 14:44:44 +0500

    Третья версия решения
    fix () for if

--- roman-time.js
+++ roman-time.js
@@ -6,7 +6,7 @@
  */
 function romanTime(time) {
     var reg = RegExp('(([01][0-9])|(2[0123]))\:[012345][0-9]')
-    if (time.length != 5) || (!reg.test(time)){
+    if ((time.length != 5) || (!reg.test(time))){
         throw new TypeError()
     }
     var romanUnits = ['', '|', '||', '|||', '|V', 'V', 'VI', 'VII', 'VIII', 'IX']

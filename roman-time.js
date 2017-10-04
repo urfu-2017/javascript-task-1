9ac667ff2d8b948e13a7af2996f5f681e1d660eb
alex43jk <leonbkone@yandex.ru>
2017-10-05 02:12:53 +0500

    Update

--- roman-time.js
+++ roman-time.js
@@ -6,14 +6,68 @@
  */
 function romanTime(time) {
     errorTime(time);
-    return time;
+    var partTime = time.split(':');
+    if (parseInt(partTime[0], 10) === 0 && parseInt(partTime[1], 10) === 0) {
+        return 'N:N';
+    }
+    if (parseInt(partTime[0], 10) === 0) {
+        var rightPart = toRomanRight(partTime[1]);
+        return 'N:' + rightPart;
+    }
+    if (parseInt(partTime[1], 10) === 0) {
+        var leftPart = toRomanLeft(partTime[0]);
+        return leftPart + ':N';
+    }
+    var leftPart = toRomanLeft(partTime[0]);
+    var rightPart = toRomanRight(partTime[1]);
+    return leftPart + ':' + rightPart;
 }
 
-function toRomanTime(arr) {
-    var hours = ['X', 'XX', 'XXX', 'XL', 'L'];
-    var minutes = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
-    
+//Левая часть времени в формате HH
+function toRomanLeft(time) {
+    var hours = ['', 'X', 'XX'];
+    var minutes = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    var result = 'N';
+    var num = time.split('');
+    for (var i = 0; i < 3; i++) {
+        if (parseInt(num[0], 10) === i) {
+            result = hours[i];
+            for (var j = 0; j < 10; j++) {
+                if (parseInt(num[1], 10) === j) {
+                    result += minutes[j];
+                    return result;
+                }
+            }
+        }
+    }
+    return result;
+    // if (parseInt(num[0], 10) === 0) {
+    //     for (var i = 1; i < 10; i++) {
+    //         if (parseInt(num[1], 10) === i) {
+    //             return minutes[i-1];
+    //         }
+    //     }
+    // }
+}
 
+//Правая часть времени в формате MM
+function toRomanRight(time) {
+    var hours = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+    var minutes = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    var result = 'N';
+    var num = time.split('');
+    for (var i = 0; i < 6; i++) {
+        if (parseInt(num[0], 10) === i) {
+            result = hours[i];
+            for (var j = 0; j < 10; j++) {
+                if (parseInt(num[1], 10) === j) {
+                    result += minutes[j];
+                    return result;
+                }
+            }
+        }
+    }
+    return result;
 }
 
 function errorTime(time) {
@@ -27,4 +81,5 @@ function errorTime(time) {
         throw new TypeError('Неверное время');
     }
 }
+
 module.exports = romanTime;

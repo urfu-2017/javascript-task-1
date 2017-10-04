ddc1a8370e2b68d039ff8f02e168c1358c19bfa4
DESKTOP-FHNEMJP\Софья <dubralex@mail.ru>
2017-10-05 01:40:46 +0500

    заменила табы на пробелы

--- roman-time.js
+++ roman-time.js
@@ -4,30 +4,42 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+function translate(arabNumber, firstNumeral) {
+    var result = '';
+    const romanDict = {
+        0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII',
+        8: 'VIII', 9: 'IX', 10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L'
+    };
+    if (firstNumeral > 9) {
+        secondNumeral = firstNumeral % 10;
+    } else {
+        secondNumeral = 0;
+    }
+    if (firstNumeral === 0) {
+        result = 'N'
+    } else {
+        result = romanDict[firstNumeral - secondNumeral] + romanDict[secondNumeral];
+    }
+
+    return result;
+}
+
 function romanTime(time) {
-	var romanDict = { 0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 
-		8: 'VIII', 9: 'IX', 10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L' };
-	var reg = new RegExp('[0-2][0-9]:([0-5])?[0-9]');
-	var result = reg.exec(time);
-	var hh = parseInt(result[0],10);
-	var mm = parseInt(result[1],10);
-	var justMinInH = hh%10;
-	var justMinInM = mm%10;
-	var roman = [];
-	if (time.length == result[0].length && hh >= 0 && hh <= 23){
-		if (hh == 0){
-			roman[0]='N';
-		}
-		if ( mm == 0){
-			roman[1]='N';
-		}
-		roman[0] = romanDict[hh - justMinInH] + romanDict[justMinInH];
-		roman[1] = romanDict[mm - justMinInM] + romanDict[justMinInM];
-	} else {
-		new TypeError();
-	}
-	time = roman.join(':');
-	return time;
+    var reg = new RegExp('([0-2])?[0-9]:([0-5])?[0-9]');
+    var rTime = reg.exec(time)[0];
+    romanDate = rTime.split(':');
+    var roman = [];
+    hh = parseInt(romanDate[0], 10);
+    mm = parseInt(romanDate[1], 10);
+    if (time.length === rTime.length && hh >= 0 && hh <= 23) {
+        roman[0] = translate(romanDate[0], hh)
+        roman[1] = translate(romanDate[1], mm)
+    } else {
+        throw new TypeError();
+    }
+    time = roman.join(':');
+
+    return time;
 }
 
 module.exports = romanTime;

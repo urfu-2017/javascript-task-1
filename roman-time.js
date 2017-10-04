ba36d223d19f03673a746a2485b39a1cc30aecea
StevePanov <32196471+StevePanov@users.noreply.github.com>
2017-10-04 11:28:37 +0300

    f1

--- roman-time.js
+++ roman-time.js
@@ -3,13 +3,13 @@ function convert(tense) {
     var smallNumbers = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
     var bigNumbers = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
     if (tense === '00') {
- 
+
         return ('N');
     }
     if (tense.lenght > 2 || tense.lenght < 2) {
         throw new TypeError('Неверное время не 2');
     } else {
- 
+
         return (bigNumbers[Math.floor(parseInt(tense) / 10)] + smallNumbers[parseInt(tense) % 10]);
     }
 }
@@ -28,7 +28,7 @@ function romanTime(time) {
     if (parseInt(time[1]) < 0 || parseInt(time[1]) > 59) {
         throw new TypeError('Неверное время: минуты');
     }
- 
+
     return (convert(time[0]) + ':' + convert(time[1]));
 }
 module.exports = romanTime;

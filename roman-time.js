352144b4abffa1cc9e2b5c2dca3d5ace008045d4
Maria Paradeeva <gentlepudding@gmail.com>
2017-10-04 23:56:05 +0500

    Solution option added

--- roman-time.js
+++ roman-time.js
@@ -4,9 +4,78 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+
+function romanTime(time) { // eslint-disable-line complexity
+    if (time === null || time === undefined) {
+        throw new TypeError('Переменная или параметр неправильного типа');
+    }
+
+    if (time.length !== 5) {
+        throw new TypeError('Данные некорректны');
+    }
+
+    var hours = parseInt(time.slice(0, 2), 10);
+    var minutes = parseInt(time.slice(3, 5), 10);
+
+    if (Number.isNaN(hours) || Number.isNaN(minutes)) {
+        throw new TypeError('Переменная или параметр неправильного типа');
+    }
+
+    if (hours >= 24 || minutes > 59) {
+        throw new TypeError('Переданный параметр находится за границами допустимого диапазона');
+    }
+
+    var hoursToRoman = romanNumber(hours);
+    var minutesToRoman = romanNumber(minutes);
+
+    return hoursToRoman + ':' + minutesToRoman;
+}
+
+function romanNumber(num) { // eslint-disable-line complexity, max-statements
+    var reminder = num;
+    var result = '';
+
+    if (num === 0) {
+        result = 'N';
+    }
+
+    if (reminder >= 50) {
+        result += 'L';
+        reminder -= 50;
+    }
+
+    if (reminder >= 40) {
+        result += 'XL';
+        reminder -= 40;
+    }
+
+    while (reminder >= 10) {
+        result += 'X';
+        reminder -= 10;
+    }
+
+    if (reminder >= 9) {
+        result += 'IX';
+        reminder -= 9;
+    }
+
+    if (reminder >= 5) {
+        result += 'V';
+        reminder -= 5;
+    }
+
+    if (reminder >= 4) {
+        result += 'IV';
+        reminder -= 4;
+    }
+
+    while (reminder >= 1) {
+        result += 'I';
+        reminder -= 1;
+    }
+
+    return result;
 }
 
 module.exports = romanTime;
+

b6cf63f3cc47beb452d55b4633858154dc77f0f2
Naturin <bontortue@gmail.com>
2017-10-04 17:35:51 +0500

    step 2.2

--- roman-time.js
+++ roman-time.js
@@ -1,84 +1,74 @@
 'use strict';
-
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
- */
+ **/
 function romanTime(time) {
     var arr = time.split(':');
     var hourArab = parseInt(arr[0]);
     var minuteArab = parseInt(arr[1]);
-    if (isNaN(hourArab) || isNaN(minuteArab) || hourArab >= 24 || hourArab < 0 || minuteArab < 0 || minuteArab > 60 ) {
-        throw new TypeError;
+    var rimNumber = '';
+    if (isNaN(hourArab) || isNaN(minuteArab)) {
+        throw new TypeError('must be digits');
     }
-    function toRoman(number)
-    {
-        var rimNumber = '';
-        var remain;
-        if (Math.floor(number/50) === 1)
-        {
+    if (hourArab >= 24 || hourArab < 0 || minuteArab < 0 || minuteArab > 60) {
+        throw new TypeError('must be hours < 24, minutes < 60');
+    }
+    var hourArabD = Math.floor(10.23);
+    var minuteArabD = Math.floor(minuteArab / 10);
+    var minuteArabO = minuteArab % 10;
+    var hourArabO = hourArab % 10;
+    function toRomanD(number) {
+        if (number === 5) {
             rimNumber = 'L';
-            remain = number % 50;
         }
-        if (Math.floor(number/10) === 4)
-        {
+        if (number === 4) {
             rimNumber = 'XL';
-            remain = number % 40;
         }
-        if (Math.floor(number/10) === 3)
-        {
-            rimNumber = 'XXX';
-            remain = number % 30;
-        }
-        if (Math.floor(number/10) === 2)
-        {
-            rimNumber = 'XX';
-            remain = number % 20;
-        }
-        if (Math.floor(number/10) === 1)
-        {
-            rimNumber = 'X';
-            remain = number % 10;
-        }
-        if (Math.floor(number/10) === 0)
-        {
-            remain = number;
-        }
-        if (remain > 5 && remain < 9)
-        {
-            while (remain > 5)
-            {
-                rimNumber += 'I';
-                remain--;
+        if (number < 4) {
+            while (number > 0) {
+                rimNumber += 'X';
+                number--;
             }
+            return rimNumber;
         }
-        if (remain === 9)
-        {
-            rimNumber += 'IX';
-        }
-        if (remain < 4)
-        {
-            while (remain !== 0)
-            {
-                rimNumber += 'I';
-                remain--;
-            }
-        }
-        if (remain === 4)
-        {
-            rimNumber += 'IV';
-        }
-        if (remain === 5)
-        {
-            rimNumber += 'V';
-        }
-        if (Math.floor(number/1) === 0)
-        {
-            rimNumber = 'NN';
-        }
-        return rimNumber;
+        return toRomanD(hourArabD);
     }
-    return toRoman(hourArab) +  ':' + toRoman(minuteArab);
+    // function toRomanO(number) {
+    //
+    // }
+    //     if (Math.floor(number / 10) === 0) {
+    //         remain = number;
+    //     }
+    //     if (remain > 5 && remain < 9) {
+    //         while (remain > 5) {
+    //             rimNumber += 'I';
+    //             remain--;
+    //         }
+    //     }
+    //     if (remain === 9) {
+    //         rimNumber += 'IX';
+    //     }
+    //     if (remain < 4) {
+    //         while (remain !== 0) {
+    //             rimNumber += 'I';
+    //             remain--;
+    //         }
+    //     }
+    //     if (remain === 4) {
+    //         rimNumber += 'IV';
+    //     }
+    //     if (remain === 5) {
+    //         rimNumber += 'V';
+    //     }
+    //     if (Math.floor(number / 1) === 0) {
+    //         rimNumber = 'NN';
+    //     }
+    //     return rimNumber;
+    // }
+
+    // return toRoman(hourArab) + ':' + toRoman(minuteArab);
+    // return rimNumber;
 }
 
 module.exports = romanTime;

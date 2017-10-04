247a764f3332df54e09284bd7123341a910c1bcd
bvhfsd <32241346+bvhfsd@users.noreply.github.com>
2017-10-04 20:49:14 +0500

    ???

--- roman-time.js
+++ roman-time.js
@@ -6,25 +6,27 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
+    var timeChecked = checkingRange(time);
+    var timeArr = timeChecked.split(':');
+    var hours = timeArr[0];
+    var minutes = timeArr[1];
+    var hoursRoman;
+    var minutesRoman;
+    hoursRoman = toRoman(hours);
+    minutesRoman = toRoman(minutes);
+
+    return hoursRoman + ':' + minutesRoman;
+}
+
+function checkingRange (time1) {
     var rangeCheck = new RegExp('((0[0-9])|(1[0-9])|(2[0-3])):[0-5][0-9]');
-    var rangeChecked = rangeCheck.test(time);
+    var rangeChecked = rangeCheck.test(time1);
     if (rangeChecked === false) {
         throw new TypeError();
-    } else {
-        var timeArr = time.split(':');
-        var hours = timeArr[0];
-        var minutes = timeArr[1];
-        var hoursRoman;
-        var minutesRoman;
-        if ((hours.length === 2) && (minutes.length === 2)) {
-            hoursRoman = toRoman(hours);
-            minutesRoman = toRoman(minutes);
-        } else {
-            throw new TypeError();
-        }
-
-        return hoursRoman + ':' + minutesRoman;
     }
+
+    return time1;
+
 }
 
 function toRoman(hh) {

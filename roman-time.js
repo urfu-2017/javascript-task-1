a96d9651cf60892ced90c518269caa745a9e19f9
ValeryZhurba <32246041+ValeryZhurba@users.noreply.github.com>
2017-10-04 20:12:19 +0300

    first try

--- roman-time.js
+++ roman-time.js
@@ -4,8 +4,52 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
+
+function checkNumber(number, min, max) {
+    if (isNaN(number) || number < min || number > max)
+        throw new TypeError();
+}
+
+function convertToRoman(nummber) {
+    var numbersArray = [50, 40, 10, 9, 5, 4, 1];
+    var romanNumerals = {
+        1 : 'I',
+        4 : 'IV',
+        5 : 'V',
+        9 : 'IX',
+        10 : 'X',
+        40 : 'XL',
+        50 : 'L'
+    };
+    var strInRoman = '';
+    var count = 0;
+    if (nummber === 0) return 'N';
+    while (count < numbersArray.length){
+        if (nummber === 0) return strInRoman;
+        var num = numbersArray[count];
+        if (nummber >= num){
+            strInRoman += romanNumerals[num];
+            nummber -= num;
+        }
+        else count++;
+    }
+    return strInRoman;
+}
+
 function romanTime(time) {
     // Немного авторского кода и замечательной магии
+    if (time === undefined || time === null)
+        throw new TypeError();
+    var numbers = time.split(':');
+    if (numbers.length === 2){
+        var hours = parseInt(numbers[0]);
+        var minutes = parseInt(numbers[1]);
+    }
+    else
+        throw new TypeError();
+    checkNumber(hours, 0 , 23);
+    checkNumber(minutes, 0 , 59);
+    time = convertToRoman(hours) + ':' + convertToRoman(minutes);
     return time;
 }
 

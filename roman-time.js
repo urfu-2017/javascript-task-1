75b549ebb702ec97f903a21a0acb4d6ce8188a35
dru <denginru@yandex.ru>
2017-10-04 12:00:21 +0500

    roman-time done

--- roman-time.js
+++ roman-time.js
@@ -1,12 +1,50 @@
 'use strict';
 
+function arabicToRoman(arabicNumber) {
+    var res = '';
+
+    if (arabicNumber === 0) {
+        return 'N';
+    }
+
+    var i = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+    var x = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+
+    var n = arabicNumber;
+
+    if (n >= 10) {
+        res += x[Math.floor(n / 10)];
+        n %= 10;
+    }
+    res += i[n];
+
+    return res;
+}
+
+function isRightTimeFormat(time) {
+    var hours = parseInt(time.split(':')[0]);
+    var minutes = parseInt(time.split(':')[1]);
+    var bad = isNaN(hours) || isNaN(minutes) || hours < 0 || hours >= 24 ||
+              minutes < 0 || minutes > 59;
+
+    return !bad;
+}
+
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function romanTime(time) {
-    // Немного авторского кода и замечательной магии
-    return time;
+    var res = '';
+    if (!isRightTimeFormat(time)) {
+        throw new TypeError('Wrong time format');
+    }
+    var timeParts = time.split(':');
+    var hours = parseInt(timeParts[0]);
+    var minutes = parseInt(timeParts[1]);
+    res = arabicToRoman(hours) + ':' + arabicToRoman(minutes);
+
+    return res;
 }
 
 module.exports = romanTime;

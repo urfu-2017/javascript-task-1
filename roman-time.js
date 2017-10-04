06d012a089c6caf125e9765b6ae4e9ddc0313568
DESKTOP-FHNEMJP\Софья <dubralex@mail.ru>
2017-10-05 01:50:01 +0500

    добавила забытые var'ы

--- roman-time.js
+++ roman-time.js
@@ -10,10 +10,9 @@ function translate(arabNumber, firstNumeral) {
         0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII',
         8: 'VIII', 9: 'IX', 10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L'
     };
+    var secondNumeral = 0;
     if (firstNumeral > 9) {
         secondNumeral = firstNumeral % 10;
-    } else {
-        secondNumeral = 0;
     }
     if (firstNumeral === 0) {
         result = 'N'
@@ -27,10 +26,10 @@ function translate(arabNumber, firstNumeral) {
 function romanTime(time) {
     var reg = new RegExp('([0-2])?[0-9]:([0-5])?[0-9]');
     var rTime = reg.exec(time)[0];
-    romanDate = rTime.split(':');
+    var romanDate = rTime.split(':');
     var roman = [];
-    hh = parseInt(romanDate[0], 10);
-    mm = parseInt(romanDate[1], 10);
+    var hh = parseInt(romanDate[0], 10);
+    var mm = parseInt(romanDate[1], 10);
     if (time.length === rTime.length && hh >= 0 && hh <= 23) {
         roman[0] = translate(romanDate[0], hh)
         roman[1] = translate(romanDate[1], mm)

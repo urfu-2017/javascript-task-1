c01fd2fe1937d610f0842f5e75ca73431f6cefcf
StevePanov <32196471+StevePanov@users.noreply.github.com>
2017-10-04 03:49:02 +0300

    newshort

--- roman-time.js
+++ roman-time.js
@@ -1,71 +1,27 @@
-'use strict'
-var smallNumbers = new Map ([
-    ['1', 'I'], ['2', 'II'], ['3', 'III'],
-    ['4', 'IV'], ['5', 'V'], ['6', 'VI'],
-    ['7', 'VII'], ['8', 'VIII'], ['9', 'IX']]);
-var bigNumbers = new Map ([['1', 'X'], ['2', 'XX'], ['3', 'XXX'], ['4', 'XL'], ['5', 'L']]);
-var romanHours;
-var romanMinutes;
-function checkFormat(hoursInInt, minInInt) {
-    if (hoursInInt === undefined || hoursInInt === null) {
-        throw new TypeError('Неверное время'); 
-    }
-    if (minInInt === undefined || minInInt === null) {
-        throw new TypeError('Неверное время'); 
-    }
-    if (hoursInInt < 0 || hoursInInt > 23 || minInInt < 0 || minInInt > 59) {
-        throw new TypeError('Неверное время'); 
-    }
-}
-function getHours(hoursInInt, hours) {
-    if (hoursInInt === 0) {
-        romanHours = 'N';
-    }
-    if (hoursInInt < 10) {
-        romanHours = smallNumbers.get(hours.charAt(1));
-    }
-    if (hoursInInt % 10 === 0) {
-        romanHours = bigNumbers.get(hours.charAt(0));
-    } else {
-        var bigHour = bigNumbers.get(hours.charAt(0));
-        var smallHour = smallNumbers.get(hours.charAt(1));
-        romanHours = bigHour + smallHour;
+'use strict';
+function convert(tense) {
+    var smallNumbers = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+    var bigNumbers = ['X', 'XX', 'XXX', 'XL', 'L'];
+    if (tense === 0) {
+
+        return ('N');
     }
 
-    return romanHours;
+    return (bigNumbers[Math.floor(parseInt(tense) / 10)] + smallNumbers[parseInt(tense) % 10]);
 }
-function getMin(minInInt, minutes) {
-    if (minInInt === 0) {
-        romanMinutes = 'N';
-    }
-    if (minInInt < 10) {
-        romanMinutes = smallNumbers.get(minutes.charAt(1));
-    }
-    if (minInInt % 10 === 0) {
-        romanMinutes = bigNumbers.get(minutes.charAt(0));
-    } else {
-        var bigMinute = bigNumbers.get(minutes.charAt(0));
-        var smallMinute = smallNumbers.get(minutes.charAt(1));
-        romanMinutes = bigMinute + smallMinute;
-    }
-
-    return romanMinutes;
-}
-function romanTime(time) {
-    if (time.length !== 5) {
+function check(tense) {
+    if (tense !== 2 || isNaN(parseInt(tense)) || tense === undefined || tense === null) {
         throw new TypeError('Неверное время');
     }
-    var hours = time.slice(0, 2);
-    var minutes = time.slice(-2);
-    var hoursInInt = parseInt(hours);
-    var minInInt = parseInt(minutes);
-    checkFormat(hoursInInt, minInInt);
-    if (isNaN(hoursInInt) || isNaN(minInInt)) {
-        throw new TypeError('Неверное время'); 
+}
+function romanTime(time) {
+    time = time.split([':']);
+    check(parseInt(time[0]));
+    check(parseInt(time[1]));
+    if (time[0] < 0 || time[0] > 23 || time[1] < 0 || time[1] > 59) {
+        throw new TypeError('Неверное время');
     }
-    getHours(hoursInInt, hours);
-    getMin(minInInt, minutes);
 
-    return (romanHours + ':' + romanMinutes);
+    return (convert(time[0]) + ':' + convert(time[1]));
 }
 module.exports = romanTime;

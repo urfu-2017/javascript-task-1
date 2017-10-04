3c71f15ef1dc9341a85baf0eb19d7fdb0a75bbcf
bvhfsd <32241346+bvhfsd@users.noreply.github.com>
2017-10-04 06:41:51 +0500

    ummmmmmmmmmmmmm

--- roman-time.js
+++ roman-time.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 /**
@@ -8,80 +9,59 @@
 // w a d d u p b i g p i m p s
 function rangeCheckHours(h) {
 
-    if (h < 0 || h > 23 || isNaN(h) || h === undefined) {
-        throw new TypeError();
-    }
+	if (h < 0 || h > 23 || isNaN(h) || h === undefined) {
+		throw new TypeError();
+	}
 
-    return h;
+	return h;
 }
 
 function rangeCheckMinutes(m) {
 
-    if (m < 0 || m > 59 || isNaN(m) || m === undefined) {
-        throw new TypeError();
-    }
+	if (m < 0 || m > 59 || isNaN(m) || m === undefined) {
+		throw new TypeError();
+	}
 
-    return m;
+	return m;
 }
 
-function hoursToRoman(hh) {
-    var firstNumArr = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
-    var secNumArr = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
-    var firstNum;
-    var secNum;
-    if (hh === 0) {
-        firstNum = 'N';
-        secNum = '';
-    } else if (hh < 10) {
-        firstNum = '';
-        secNum = secNumArr[hh];
-    } else {
-        firstNum = firstNumArr[parseInt((hh / 10), 10)];
-        secNum = secNumArr[hh % 10];
-    }
+function toRoman(hh) {
+	var firstNumArr = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+	var secNumArr = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+	var firstNum;
+	var secNum;
+	if (hh === 0) {
+		firstNum = 'N';
+		secNum = '';
+	} else if (hh < 10) {
+		firstNum = '';
+		secNum = secNumArr[hh];
+	} else {
+		firstNum = firstNumArr[parseInt((hh / 10), 10)];
+		secNum = secNumArr[hh % 10];
+	}
 
-    return [firstNum + secNum];
-
-}
-
-function minutesToRoman(mm) {
-    var firstNumArr = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
-    var secNumArr = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
-    var firstNum;
-    var secNum;
-    if (mm === 0) {
-        firstNum = 'N';
-        secNum = '';
-    } else if (mm < 10) {
-        firstNum = '';
-        secNum = secNumArr[mm];
-    } else {
-        firstNum = firstNumArr[parseInt((mm / 10), 10)];
-        secNum = secNumArr[mm % 10];
-    }
-
-    return [firstNum + secNum];
+	return [firstNum + secNum];
 
 }
 
 function romanTime(time) {
-    var hoursRoman;
-    var minutesRoman;
-    var timeArr = time.split(':');
-    var hours = timeArr[0];
-    var minutes = timeArr[1];
-    if ((hours.length === 2) && (minutes.length === 2)) {
-        var hoursParsed = parseInt (hours, 10);
-        var minutesParsed = parseInt (minutes, 10);
-        var hoursRangeChecked = rangeCheckHours (hoursParsed);
-        var minutesRangeChecked = rangeCheckMinutes (minutesParsed);
-        hoursRoman = hoursToRoman(hoursRangeChecked);
-        minutesRoman = minutesToRoman(minutesRangeChecked);
-    } else {
-        throw new TypeError();
-    }
+	var hoursRoman;
+	var minutesRoman;
+	var timeArr = time.split(':');
+	var hours = timeArr[0];
+	var minutes = timeArr[1];
+	if ((hours.length === 2) && (minutes.length === 2)) {
+		var hoursParsed = parseInt (hours, 10);
+		var minutesParsed = parseInt (minutes, 10);
+		var hoursRangeChecked = rangeCheckHours (hoursParsed);
+		var minutesRangeChecked = rangeCheckMinutes (minutesParsed);
+		hoursRoman = toRoman(hoursRangeChecked);
+		minutesRoman = toRoman(minutesRangeChecked);
+	} else {
+		throw new TypeError();
+	}
 
-    return [hoursRoman] + ':' + [minutesRoman];
+	return [hoursRoman] + ':' + [minutesRoman];
 }
 module.exports = romanTime;
-

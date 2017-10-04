79d7449424da8a954886ee92edcda46a94b53241
violetapple <varabei45@gmail.com>
2017-10-04 22:35:48 +0600

    refactor

--- roman-time.js
+++ roman-time.js
@@ -5,10 +5,7 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 function isCorrectTime(time) {
-    if (time === undefined) {
-        return false;
-    }
-    if (time === null && typeof(a) === 'object') {
+    if (time === undefined || (time === null && typeof(a) === 'object')) {
         return false;
     }
     var result = time.match(/[0-9][0-9]:[0-9][0-9]/ig);
@@ -16,45 +13,34 @@ function isCorrectTime(time) {
         return false;
     }
     var timeParts = time.split(':');
-    if (timeParts.length !== 2) {
-        return false;
-    }
-    if (timeParts[0] % 1 !== 0 || timeParts[1] % 1 !== 0) {
+    if (timeParts.length !== 2 || (timeParts[0] % 1 !== 0 || timeParts[1] % 1 !== 0)) {
         return false;
     }
     var hours = parseInt(timeParts[0]);
     var minutes = parseInt(timeParts[1]);
-    if (hours < 0 || hours > 23) {
-        return false;
-    }
-    if (minutes < 0 || minutes > 59) {
+    if (hours < 0 || hours > 23 || minutes < 0 || minutes > 59) {
         return false;
     }
 
     return true;
 }
 
-function timeToRoman(timeString) {
-
-    /**
-    * @param {String} time – корректное время-строка в формате HH:MM (например, 09:05)
-    */
-    var romanDict = { 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII',
-                 8: 'VIII', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L' };
-    var timeParts = timeString.split(':');
-    var hours = parseInt(timeParts[0]);
-    var minutes = parseInt(timeParts[1]);
-    var romanHours = '';
-    var romanMinutes = '';
+function hoursToRoman(hours, romanDict) {
+    var romanHours;
     if (hours === 0) {
         romanHours += 'N';
     } else {
-        for (var i = 0; i < parseInt(hours / 10); i ++) {
+        var i;
+        for (i = 0; i < parseInt(hours / 10); i++) {
             romanHours += romanDict[10];
         }
         romanHours += romanDict[hours % 10];
     }
+    return romanHours;
+}
 
+function minutesToRoman(minutes, romanDict) {
+    var romanMinutes;
     if (minutes === 0) {
         romanMinutes += "N";
     } else {
@@ -71,8 +57,19 @@ function timeToRoman(timeString) {
             romanMinutes += romanDict[minutes % 10];
         }
     }
+}
+function timeToRoman(timeString) {
 
-    return romanHours + ':' + romanMinutes;
+    /**
+    * @param {String} time – корректное время-строка в формате HH:MM (например, 09:05)
+    */
+    var romanDict = { 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII',
+                 8: 'VIII', 9: 'IX', 10: 'X', 40: 'XL', 50: 'L' };
+    var timeParts = timeString.split(':');
+    var hours = parseInt(timeParts[0]);
+    var minutes = parseInt(timeParts[1]);
+
+    return hoursToRoman(hours, romanDict) + ':' + minutesToRoman(minutes, romanDict);
 }
 
 function romanTime(time) {
@@ -80,7 +77,7 @@ function romanTime(time) {
     if (!isCorrectTime(time)) {
         throw new TypeError();
     }
-    
+ 
     return timeToRoman(time);
 }
 

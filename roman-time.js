7ecee6497fbedcca52c1a367e68f95e523325f29
StevePanov <32196471+StevePanov@users.noreply.github.com>
2017-10-04 11:21:30 +0300

    finally

--- roman-time.js
+++ roman-time.js
@@ -1,27 +1,34 @@
 'use strict';
 function convert(tense) {
-    var smallNumbers = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
-    var bigNumbers = ['X', 'XX', 'XXX', 'XL', 'L'];
-    if (tense === 0) {
-
+    var smallNumbers = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+    var bigNumbers = ['', 'X', 'XX', 'XXX', 'XL', 'L'];
+    if (tense === '00') {
+ 
         return ('N');
     }
-
-    return (bigNumbers[Math.floor(parseInt(tense) / 10)] + smallNumbers[parseInt(tense) % 10]);
+    if (tense.lenght > 2 || tense.lenght < 2) {
+        throw new TypeError('Неверное время не 2');
+    } else {
+ 
+        return (bigNumbers[Math.floor(parseInt(tense) / 10)] + smallNumbers[parseInt(tense) % 10]);
+    }
 }
 function check(tense) {
-    if (tense !== 2 || isNaN(parseInt(tense)) || tense === undefined || tense === null) {
+    if (isNaN(tense) || tense === undefined || tense === null) {
         throw new TypeError('Неверное время');
     }
 }
 function romanTime(time) {
     time = time.split([':']);
-    check(parseInt(time[0]));
-    check(parseInt(time[1]));
-    if (time[0] < 0 || time[0] > 23 || time[1] < 0 || time[1] > 59) {
-        throw new TypeError('Неверное время');
+    check(time[0]);
+    check(time[1]);
+    if (parseInt(time[0]) < 0 || parseInt(time[0]) > 23) {
+        throw new TypeError('Неверное время: часы');
     }
-
+    if (parseInt(time[1]) < 0 || parseInt(time[1]) > 59) {
+        throw new TypeError('Неверное время: минуты');
+    }
+ 
     return (convert(time[0]) + ':' + convert(time[1]));
 }
 module.exports = romanTime;

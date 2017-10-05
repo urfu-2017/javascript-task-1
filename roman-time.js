7206ff0f5ad5d7ba9ae16f9c53dfb15026b4dbe2
maffas <psp720@mai.ru>
2017-10-05 18:43:16 +0500

    Моё решение задачи

--- roman-time.js
+++ roman-time.js
@@ -4,35 +4,34 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function romanTime(time) {
-    if (typeof time != 'string') {
+function romanTime (time) {
+    if (typeof time !== 'string') {
         throw new TypeError("время должно быть строкой!");
     }
-
     var arr= time.split(':');
-    if( arr[0]>=24 || arr[1]>=60 || isNaN(parseInt(arr[0],10)) || isNaN(parseInt(arr[1],10)) )
+    if (arr[0]>=24 || arr[1]>=60 || isNaN(parseInt(arr[0], 10)) || isNaN(parseInt(arr[1], 10)))
     {
         throw new TypeError("неверное время!");
     } 
-    else 
+    else
     {
         var flag = false;
         time="";
-        var rome = [["I",1], ["IV",4], ["V",5], ["IX",9], ["X",10], ["XL",40], ["L",50]];
-        for( var i=0; i<=1;i++){
-            if(arr[i]==0)
+        var rome = [["I", 1], ["IV", 4], ["V", 5], ["IX", 9], ["X", 10], ["XL", 40], ["L", 50]];
+        for(var i=0;i<=1;i++){
+            if (arr[i]===0)
             {
                 time+="N";
             }
-             for(var j=rome.length-1;arr[i]>0; j-- ){
-             if(arr[i]-rome[j][1]>=0)
+             for (var j=rome.length-1;arr[i]>0; j-- ){
+             if (arr[i]-rome[j][1]>=0)
              {
                 arr[i]-=rome[j][1];
                 time+=rome[j][0];
                 j=rome.length-1
              }
             }
-            if(!flag){
+            if (!flag){
              time+=":";
              flag=true;
             }

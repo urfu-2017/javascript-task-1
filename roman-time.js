86dfbd5333055d95b10f990707a45646bef46813
Vsevolod Tsilyurik <cilyrik.velfr@gmail.com>
2017-10-05 10:03:57 +0500

    roman-time update

--- roman-time.js
+++ roman-time.js
@@ -23,8 +23,8 @@
  * @returns {String} – время римскими цифрами (IX:V)
  */
 
-var tensRom = { 0:"", 10:"X", 20:"XX", 30:"XXX", 40:"XL", 50:"L"};
-var onesRom = { 0:"N", 1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX" };
+const tensRom = { 0:"", 10:"X", 20:"XX", 30:"XXX", 40:"XL", 50:"L"};
+const onesRom = { 0:"N", 1:"I", 2:"II", 3:"III", 4:"IV", 5:"V", 6:"VI", 7:"VII", 8:"VIII", 9:"IX" };
 
 function decToRom(decimal) {
     var roman = "";

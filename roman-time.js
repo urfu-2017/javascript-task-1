eb63927b7c9b0460fe0b03ec95dffd0ec5384d1b
Валерий Мифтахов <7978593@gmail.com>
2017-10-05 15:18:48 +0500

    Вторая попытка

--- roman-time.js
+++ roman-time.js
@@ -8,20 +8,18 @@ var romanNumberTen = ['N', 'X', 'XX', 'XXX', 'XL', 'L'];
 var romanNumber = ['N', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
 
 function romanTime(time) {
-    var hour = parseInt(time.slice(0, 2), 10);
-    var minute = parseInt(time.slice(3, 5), 10);
-    if (time.length !== 5 ||
-        time === (null || undefined) ||
-        Number.isNaN(hour) ||
-        Number.isNaN(minute) ||
-        hour >= 24 ||
-        minute >= 60) {
+    let re = /^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/;
+    if (!re.test(time)) {
         throw new TypeError('TimeError');
     } else {
-        var romanTimeHour = romanNumberTen[hour / 10] + romanNumber[hour % 10];
-        var romanTimeMinute = romanNumberTen[minute / 10] + romanNumber[minute % 10];
+        var hour = parseInt(time.slice(0, 2), 10);
+        var minut = parseInt(time.slice(3, 5), 10);
+        var romanTimeHour = (romanNumberTen[(hour - hour % 10) / 10] +
+         romanNumber[hour % 10]).replace('N', '');
+        var romanTimeminut = (romanNumberTen[(minut - minut % 10) / 10] +
+         romanNumber[minut % 10]).replace('N', '');
 
-        return romanTimeHour + ':' + romanTimeMinute;
+        return romanTimeHour + ':' + romanTimeminut;
     }
 }
 

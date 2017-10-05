f2c07669995b775d6c2b8871c5051fc71b5ea3f7
Тамара <32191326+taff42@users.noreply.github.com>
2017-10-05 18:20:58 +0500

    upd[2]

--- roman-time.js
+++ roman-time.js
@@ -6,6 +6,10 @@
  */
 function romanTime(time) {
         // Немного авторского кода и замечательной магии
+    var regexp = /^(([0,1][0-9])|(2[0-3])):[0-5][0-9]$/;
+    if (regexp.test(time) === 0) {
+        throw new TypeError('Неверное время');
+    }
     var strTime;
     strTime = time.split(':');
     var firstArray = parseInt(strTime[0], 10);
@@ -18,9 +22,9 @@ function getHours(firstArray) {
     var Unit = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
     var Ten = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX'];
     if (firstArray >= 1 && firstArray <= 23) {
-        firstArray %= 100; // 2,5
-        romH += Ten[Math.floor(firstArray / 10)]; // 2
-        firstArray %= 10; // 5
+        firstArray %= 100;
+        romH += Ten[Math.floor(firstArray / 10)];
+        firstArray %= 10;
         romH += Unit[firstArray];
     } else if (firstArray === 0) {
         romH += 'N';
@@ -35,9 +39,9 @@ function getMinutes(secondArray) {
     var Unit = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
     var Ten = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX'];
     if (secondArray >= 1 && secondArray <= 60) {
-        secondArray %= 100; // 2,5
-        romM += Ten[Math.floor(secondArray / 10)]; // 2
-        secondArray %= 10; // 5
+        secondArray %= 100;
+        romM += Ten[Math.floor(secondArray / 10)];
+        secondArray %= 10;
         romM += Unit[secondArray];
     } else if (secondArray === 0) {
         romM += 'N';

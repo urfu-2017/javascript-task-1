5da3e7a6162844cb6b1d708eeae30de096c70f14
strota <32236607+strota@users.noreply.github.com>
2017-10-05 13:49:26 +0500

    Gthtpfkbd//

--- roman-time.js
+++ roman-time.js
@@ -16,17 +16,16 @@ function romanTime(time) {
         { symbol: 'I', value: 1 }
     ];
     time = time.split(':');
-    if ((Number(time[0]) > 23) || (Number(time[1]) > 59)){
-        return 'неверное время';
-    }
-    if ((time[0].length > 2) || (time[1].length > 2)){
+    time[0] = Number(time[0]);
+    time[1] = Number(time[1]);
+    if ((time[0] > 23) || (time[1] > 59) || (time[0].length > 2) || (time[1].length > 2)) {
         return 'неверное время';
     }
     var line = test(time);
-    if (line === 'false'){
+    if (line === 'false') {
         return "неверное время"; 
     }
-    for (var e = 0; e < 3; e++){
+    for (var e = 0; e < 3; e++) {
         line = roman(Number(time[e]), rules, line);
     }
 
@@ -34,8 +33,8 @@ function romanTime(time) {
 }
 
 function test(time) {
-    for (var i = 0; i < 3; i++){
-        if ((Number(time[i]) === null) || (Number(time[i]) === undefined) || (Number(time[i] === NaN))){
+    for (var i = 0; i < 3; i++) {
+        if ((time[i] === null) || (time[i] === undefined) || (isNaN(time[i]))) {
             return 'false';
         }
 
@@ -45,20 +44,17 @@ function test(time) {
 }
 
 function roman(time, rules, line) {
-    if (time === 0){
+    if (time === 0) {
         line += 'N';
     }
     for (var i = 0; i < rules.length; i++) {
         var value = rules[i].value;
         var symbol = rules[i].symbol;
-
         var repeat = Math.floor(time / value);
-
         for (var j = 0; j < repeat; j++) {
             line += symbol;
             time -= value;
-        }
-        
+        }   
     }
     line += ':';
 

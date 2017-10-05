24d84a724a354f2f1278f5c4a0ae4a3c8e385f57
Vsevolod Tsilyurik <cilyrik.velfr@gmail.com>
2017-10-05 10:04:55 +0500

    roman-time update

--- roman-time.js
+++ roman-time.js
@@ -1,23 +1,5 @@
 'use strict';
 
-/*
-  0 = N
-  1 = I
-  2 = II
-  3 = III
-  4 = IV
-  5 = V
-  6 = VI
-  7 = VII
-  8 = VIII
-  9 = IX
-  10 = X
-  20 = XX
-  30 = XXX
-  40 = XL
-  50 = L
- */
-
 /**
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)

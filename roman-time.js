b419aed7020cb7548378865fbecd458d23398ad4
impuppy <31798367+impuppy@users.noreply.github.com>
2017-10-09 06:31:57 +0500

    Update roman-time.js

--- roman-time.js
+++ roman-time.js
@@ -4,52 +4,45 @@
  * @param {String} time – время в формате HH:MM (например, 09:05)
  * @returns {String} – время римскими цифрами (IX:V)
  */
-function romanTime(time) 
+function romanTime(time)
 {
-    ErrorCheck(time);
-    
+    errorCheck(time);
+
     var InNumbers = time.split(':');
     var RoNumbers = ['',''];
 
-    for (var j = 0; j < 2; j++)
-    {
-        RoNumbers[j] = ConvertNumbers(InNumbers[j], j);        
+    for (var j = 0; j < 2; j++){
+        RoNumbers[j] = сonvertNumbers(InNumbers[j], j);   
     }
 
-   time = RoNumbers.join(':');
+    time = RoNumbers.join(':');
 
     return time;
 }
-//Проверяем, правильно ли задано время, если неправильно, возвращаем TypeError с сообщением "Задано неверное время"
-function ErrorCheck(time)
-{
+// Проверяем, правильно ли задано время, если неправильно, возвращаем TypeError с сообщением "Задано неверное время"
+function errorCheck(time){
     var TestTime = time.split(':');
-//Проверка на вхождение заданного числа в диапазон с 00:00 до 23:59
-    if ((0 <= Number(TestTime[0])) && (23 >= Number(TestTime[0])) && (0 <= Number(TestTime[1])) && (59 >= Number(TestTime[1])))
-    {
+
+    if ((Number(TestTime[0]) >= 0) && (Number(TestTime[0]) <= 23) && 
+    (Number(TestTime[1]) >= 0) && (Number(TestTime[1]) <= 59)){
 
         return time;
     }
-    else
-    {
-        throw new TypeError('Задано неверное время');
-    }
+    throw new TypeError('Задано неверное время');    
 }
 
-function ConvertNumbers (ArNumbers, j)
-{
-    var RoNumbers = ['',''];
-    RoNumbers[1] = ArNumbers%10;
-    RoNumbers[0] = parseInt(ArNumbers/10);
+function сonvertNumbers (ArNumbers, j){
+    var RoNumbers = ['', ''];
+    RoNumbers[1] = ArNumbers % 10;
+    RoNumbers[0] = parseInt(ArNumbers / 10);
 
-    if ((RoNumbers[0] === 0) && (RoNumbers[1] === 0))
-    {
+    if ((RoNumbers[0] === 0) && (RoNumbers[1] === 0)){
         ArNumbers = 'N';
+
         return ArNumbers;
     }
-    //перевод единиц в римские цифры
-    switch (RoNumbers[1])
-    {
+
+    switch (RoNumbers[1]){
         case 0:
             RoNumbers[1] = 'N';
             break;
@@ -84,56 +77,30 @@ function ConvertNumbers (ArNumbers, j)
             throw new TypeError('Неверно задано время');
             break;
     }
-    //если первая часть числа ноль, а второе не ноль, то возвращаем вторую часть числа 
-    if ((RoNumbers[0] === 0) && (RoNumbers[1] !== null))
-    {
+
+    if ((RoNumbers[0] === 0) && (RoNumbers[1] !== null)){
 
         return RoNumbers[1];
     }
-    //проверка на ноль
-    if((RoNumbers[0] > 0) && (RoNumbers[1] === 'N'))
-    {
-        RoNumbers[1] == null;
-    }
-    if(RoNumbers[0] == 0)
-    {
-        RoNumbers[0] == null;
-    }
-    //перевод десятков в римские цифры
-    if(j === 0)//часы
-    {
-        if (RoNumbers[0] === 1)
-        {
+    
+    switch(RoNumbers[0]){
+    case 1:
             RoNumbers[0] = 'X';
-        }
-        if (RoNumbers[0] === 2)
-        {
+            break;
+        case 2:
             RoNumbers[0] = 'XX';
-        }
-    }
-    else//минуты
-    {
-        switch(RoNumbers[0])
-        {
-            case 1:
-                RoNumbers[0] = 'X';
-                break;
-            case 2:
-                RoNumbers[0] = 'XX';
-                break;
-            case 3:
-                RoNumbers[0] = 'XXX';
-                break;
-            case 4:
-                RoNumbers[0] = 'XL';
-                break;
-            case 5:
-                RoNumbers[0] = 'L';
-                break;
-            default:
-                throw new TypeError('');
-                break;
-        }
+            break;
+        case 3:
+            RoNumbers[0] = 'XXX';
+            break;
+        case 4:
+            RoNumbers[0] = 'XL';
+            break;
+        case 5:
+            RoNumbers[0] = 'L';
+            break;
+        default:
+            throw new TypeError('');
     }
     RoNumbers = RoNumbers.join('');
 
